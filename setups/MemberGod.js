2a8f9d931eedbfbf6c4e24e04edd6971f073f93a
ArcerionDev <arcerion114@gmail.com>
2020-01-31 16:54:41 -0500

    BUGGED

---
+++ setups/MemberGod.js
@@ -0,0 +1,7 @@
+PIXI.game.prodigy.player.tt = true; // Set Membership
+PIXI.game.prodigy.player.modifiers.damage=100; // 100x Damage
+PIXI.game.prodigy.player.modifiers.maxHearts=100; // 100x HP
+PIXI.game.prodigy.debugMisc.setGameSpeed(10) // 10x walking speed, 100x is annoying to look at
+
+// TODO: Investigate why easy mode breaks battles
+PIXI.game.prodigy.debugMisc.easyMode(1, 100); // Easy mode, 100% correct
