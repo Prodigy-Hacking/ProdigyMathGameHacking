a4d456c251ffaead5fb1904bc1f713e18faf3376
Avn1114 <60945964+Avn1114@users.noreply.github.com>
2020-02-12 16:15:13 -0500

    Create RichieRich.js

---
+++ setups/RichieRich.js
@@ -0,0 +1,16 @@
+// Gives player 99 conjure cubes. (Any more causes the game to crash.)
+PIXI.game.prodigy.debugMisc.getCubes(99)
+PIXI.game.prodigy.create.conjureCubeButton()
+//Sets the amount of all your currencies to 9 million.
+x = PIXI.game.prodigy.player.backpack.data.currency
+for (i in x) {
+    x[i] = {"ID": x[i].ID, "N": 9000000}
+}
+PIXI.game.prodigy.debugMisc.getAllPotions(999999999999)
+PIXI.game.prodigy.debugMisc.smallLoan(1000000000)
+//Sets the amount of all your currencies to 9 million.
+x = PIXI.game.prodigy.player.backpack.data.item
+for (i in x) {
+    x[i] = {"ID": x[i].ID, "N": 9000000}
+}
+PIXI.game.prodigy.player.tt=true
