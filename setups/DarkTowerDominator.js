688440d6b517921325486e71d1b6806b30fa3300
Gale <>
2020-02-12 16:02:57 -0500

    Create DarkTowerDominator.js

---
+++ setups/DarkTowerDominator.js
@@ -0,0 +1,5 @@
+// Use this right after you get into the Dark Tower!
+PIXI.game.prodigy.debugMisc.tpTowerFloor(100) 
+PIXI.game.prodigy.player.tt = true; // Set Membership
+PIXI.game.prodigy.player.modifiers.damage=1000; // 1000x Damage
+PIXI.game.prodigy.player.modifiers.maxHearts=1000; // 1000x HP
