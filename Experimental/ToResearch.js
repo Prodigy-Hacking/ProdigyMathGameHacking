ad40eb0a5812aa263c719716bca5d1457b21f6c0
Mustan Pathetic <patheticmustan@gmail.com>
2020-02-12 11:34:48 -0500

    Update and rename Collab request to Experimental/ToResearch.js

---
+++ Experimental/ToResearch.js
@@ -0,0 +1,15 @@
+// Lists interesting functions/objects we should look into.
+
+
+
+// Gives all key items to player, other than Academy Amulet.
+PIXI.game.prodigy.debugMisc.getKeyItems()
+
+//Gives 100 Bounty points. For some reason, it doesn't work with more than 100 at a time.
+PIXI.game.prodigy.debugMisc.getBounty(100)
+
+// Heals team. (EXPERIMENTAL)
+PIXI.game.prodigy.player.healTeam(9999999)
+
+// It seems like there are a lot of debug functions left in here by the developers...
+PIXI.game.prodigy.debugMisc.prototype
