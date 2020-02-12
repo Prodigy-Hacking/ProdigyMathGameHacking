595bbda7e745ed03db87e099062c992cc1d453c3
Avn1114 <60945964+Avn1114@users.noreply.github.com>
2020-02-12 16:08:28 -0500

    Update DarkTowerDominator.js

--- setups/DarkTowerDominator.js
+++ setups/DarkTowerDominator.js
@@ -3,3 +3,4 @@ PIXI.game.prodigy.debugMisc.tpTowerFloor(100)
 PIXI.game.prodigy.player.tt = true; // Set Membership
 PIXI.game.prodigy.player.modifiers.damage=1000; // 1000x Damage
 PIXI.game.prodigy.player.modifiers.maxHearts=1000; // 1000x HP
+PIXI.game.prodigy.player.heal(9999999999999999999999999999999999999999999999)
