f33290996b4795bce954b368ee87c19ff937865d
ArcerionDev <arcerion114@gmail.com>
2020-02-12 14:47:43 -0500

    Update AcademyUnlock.js

--- Experimental/AcademyUnlock.js
+++ Experimental/AcademyUnlock.js
@@ -1,1 +1,2 @@
 PIXI.game.prodigy.debugQuests.unlockAcademy() // Unlocks the Academy
+// UPDATE: This doesn't let you unlock the academy before level 15, but it lets you unlock it without needing to enter a battle or go through the cutscene and such.
