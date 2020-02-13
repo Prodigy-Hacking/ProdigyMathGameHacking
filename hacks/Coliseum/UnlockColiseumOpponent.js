3e0b4326f3d70843d3b02e72deac705a927059d5
Avn1114 <60945964+Avn1114@users.noreply.github.com>
2020-02-13 13:50:59 -0500

    Create UnlockColiseumOpponent.js

---
+++ hacks/Coliseum/UnlockColiseumOpponent.js
@@ -0,0 +1,3 @@
+// Unlock any Coliseum opponent with a number. (1= first opponent, 2= second opponent, etc.)
+// You can use LockColiseumOpponent.js to access Coliseum members previous to the ones you unlock.
+PIXI.game.prodigy.debugMisc.unlockColiseumOpponent()
