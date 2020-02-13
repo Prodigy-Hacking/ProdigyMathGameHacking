8d75863e7d90b6ed7b61a4148ae76a179a1daa99
Gale <>
2020-02-13 10:08:28 -0500

    Create FillCombatEnergy.js

---
+++ hacks/FillCombatEnergy.js
@@ -0,0 +1,3 @@
+// Use this during combat, and your energy will fill up to the maximum, allowing you to use all=out attacks as much as you want.
+//Just make sure to re-paste this script at the beginning of every battle.
+PIXI.game.prodigy.debugMisc.setBattleEnergy(10)
