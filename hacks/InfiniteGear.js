5a3249e0e52a56d1ee382d06247e920067ee17b2
ArcerionDev <arcerion114@gmail.com>
2020-02-14 13:59:20 -0500

    Update InfiniteGear.js

--- hacks/InfiniteGear.js
+++ hacks/InfiniteGear.js
@@ -1,4 +1,16 @@
-// Gives player a lot of gear.
-for (let i = 0; i < 5000; i++) {
- PIXI.game.prodigy.debugMisc.grabBag(50);
+//Unlocks all equipment.
+PIXI.game.prodigy.player.backpack.data.outfit=[]
+x = PIXI.game.state.states.Boot._gameData.outfit
+for (i in x) {
+    PIXI.game.prodigy.player.backpack.data.outfit[i] = {"ID": x[i].ID, "N": 1}
+}
+PIXI.game.prodigy.player.backpack.data.hat=[]
+x = PIXI.game.state.states.Boot._gameData.hat
+for (i in x) {
+    PIXI.game.prodigy.player.backpack.data.hat[i] = {"ID": x[i].ID, "N": 1}
+}
+PIXI.game.prodigy.player.backpack.data.boots=[]
+x = PIXI.game.state.states.Boot._gameData.boots
+for (i in x) {
+    PIXI.game.prodigy.player.backpack.data.boots[i] = {"ID": x[i].ID, "N": 1}
 }
