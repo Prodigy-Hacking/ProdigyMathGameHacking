3b3b8b5e40ca5e1b7f97fac56fb964af9f58fea2
Avn1114 <60945964+Avn1114@users.noreply.github.com>
2020-02-14 14:14:09 -0500

    Create BecomeAGod.js

---
+++ hacks/BecomeAGod.js
@@ -0,0 +1,4 @@
+// Gives you infinite health/ damage.
+PIXI.game.prodigy.player.modifiers.damage=Infinity;
+PIXI.game.prodigy.player.modifiers.maxHearts=Infinity;
+PIXI.game.prodigy.player.heal(Infinity)
