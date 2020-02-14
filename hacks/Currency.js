0adf266b9147f0f9de112468a5f7834351d7f0c2
Avn1114 <60945964+Avn1114@users.noreply.github.com>
2020-02-14 08:27:34 -0500

    Update Currency.js

--- hacks/Currency.js
+++ hacks/Currency.js
@@ -1,3 +1,9 @@
+// Gives all currencies.
+PIXI.game.prodigy.player.backpack.data.currency=[]
+x = PIXI.game.state.states.Boot._gameData.currency
+for (i in x) {
+    PIXI.game.prodigy.player.backpack.data.currency[i] = {"ID": x[i].ID, "N": 999999999}
+}
 //Sets the amount of all your currencies to 9 million.
 x = PIXI.game.prodigy.player.backpack.data.currency
 for (i in x) {
