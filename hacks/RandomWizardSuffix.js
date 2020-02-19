0a0d23129aa9d03454907e96940d08a874842085
Gale <>
2020-02-19 14:02:55 -0500

    Create RandomWizardSuffix.js

---
+++ hacks/RandomWizardSuffix.js
@@ -0,0 +1,3 @@
+// Gives you a new wizard suffix (i.e. Nightcatcher, Quakecaller, Angelbreath, etc.)
+PIXI.game.prodigy.player.appearance._name.lastName=(Math.round(Math.random() * 1000))
+PIXI.game.prodigy.player.appearance._name.middleName=(Math.round(Math.random() * 1000))
