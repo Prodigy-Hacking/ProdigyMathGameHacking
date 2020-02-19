a41cfc332cb6b0fee50889d09ec3fc1031944be3
Gale <>
2020-02-19 17:56:21 -0500

    Create getAllBuddies.js

---
+++ hacks/getAllBuddies.js
@@ -0,0 +1,6 @@
+// Gives you all buddies.
+PIXI.game.prodigy.player.backpack.data.follow=[]
+x = PIXI.game.state.states.Boot._gameData.follow
+for (i in x) {
+    PIXI.game.prodigy.player.backpack.data.follow[i] = {"ID": x[i].ID, "N": 1}
+}
