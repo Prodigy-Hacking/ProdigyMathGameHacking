e52f5c694084119aa380cfc947b7cdc2d4ea6663
ArcerionDev <arcerion114@gmail.com>
2020-02-20 10:56:37 -0500

    Update UpgradeHeartBonus.js

--- hacks/UpgradeHeartBonus.js
+++ hacks/UpgradeHeartBonus.js
@@ -1,6 +1,6 @@
 // Upgrades the bonuses of your gear.
 x = PIXI.game.state.states.Boot._gameData.affix
 for (i in x) {
-    PIXI.game.state.states.Boot._gameData.affix[i].data.value=Infinity
-PIXI.game.state.states.Boot._gameData.affix[i].data.valuePercent=Infinity
+    PIXI.game.state.states.Boot._gameData.affix[i].data.value=100000
+PIXI.game.state.states.Boot._gameData.affix[i].data.valuePercent=100000
 }
