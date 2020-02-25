3c50c8bfb41c5fa1b368aa706091e721e9007b2d
I am nobody <60945964+Avn1114@users.noreply.github.com>
2020-02-25 14:06:41 -0500

    Update AddCustomChat.js

--- Experimental/AddCustomChat.js
+++ Experimental/AddCustomChat.js
@@ -1,3 +1,3 @@
 // Allows you to make custom chat. (Use responsibly!)
-PIXI.game.prodigy.chat.phrases.push(prompt("What would you like to say?"))
 PIXI.game.prodigy.chat.phrases.push-1
+PIXI.game.prodigy.chat.phrases.push(prompt("What would you like to say?"))
