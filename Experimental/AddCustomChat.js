7c169b0a3a77848967f708bf1acb33aa6f28d6ce
Mustan Pathetic <patheticmustan@gmail.com>
2020-02-26 08:45:54 -0500

    Bruh add quotes

--- Experimental/AddCustomChat.js
+++ Experimental/AddCustomChat.js
@@ -1,5 +1,5 @@
 // Allows you to make custom chat. (Use responsibly!)
 PIXI.game.prodigy.chat.phrases.push(prompt("What would you like to say?"));
-alert("Your message has been added. Check in the chat bar!);
+alert("Your message has been added. Check in the chat bar!");
 // Use the script below to make another custom chat message!
-PIXI.game.prodigy.chat.phrases.push-1;
+PIXI.game.prodigy.chat.phrases.push("Custom Message");
