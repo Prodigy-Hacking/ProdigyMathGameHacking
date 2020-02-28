9b0f07d2ce4cc4f914ef8fd5be6338c6844dfe74
I am nobody <60945964+Avn1114@users.noreply.github.com>
2020-02-28 11:57:25 -0500

    Create QuestKeywords.js

---
+++ Experimental/QuestKeywords.js
@@ -0,0 +1,10 @@
+// Keywords for quest auto-completion. (Don't paste this into the console. It won't do anything.)
+forest
+shiverchill
+skywatch
+bonfire_spire
+dyno
+lamplight
+academy
+earthtower
+shipwreck_shore
