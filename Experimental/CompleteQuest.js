fa01a88919e2cf67a3e2a40de9027abde273142d
I am nobody <60945964+Avn1114@users.noreply.github.com>
2020-02-28 11:58:06 -0500

    Create CompleteQuest.js

---
+++ Experimental/CompleteQuest.js
@@ -0,0 +1,2 @@
+// Completes quest requirements. For the keywords, go to https://raw.githubusercontent.com/PatheticMustan/ProdigyMathGameHacking/master/Experimental/QuestKeywords.js
+PIXI.game.prodigy.debugQuests.completeQuestRequirements(" ")
