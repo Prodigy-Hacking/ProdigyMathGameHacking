17a99e375367ec0ff8b67b30ebda10530a45f91d
I am nobody <60945964+Avn1114@users.noreply.github.com>
2020-02-28 13:37:40 -0500

    Update ChangeChatFont.js

--- DIY/ChangeChatFont.js
+++ DIY/ChangeChatFont.js
@@ -1,3 +1,20 @@
 // Changes chat font (and color) Created by @Magmischief.
 PIXI.game.prodigy.user.chatText.Font="'Comic Sans MS'"
 PIXI.game.prodigy.user.chatText.FillColor="#ffffff"
+// Other chat colors:
+//White 
+PIXI.game.prodigy.user.chatText.FillColor="#FFFFFF"
+//Red 
+PIXI.game.prodigy.user.chatText.FillColor="#FF0000"
+//Silver 
+PIXI.game.prodigy.user.chatText.FillColor="#C0C0C0"
+//Gray 
+PIXI.game.prodigy.user.chatText.FillColor="#808080"
+//Black 
+PIXI.game.prodigy.user.chatText.FillColor="#000000"
+//Green 
+PIXI.game.prodigy.user.chatText.FillColor="#008000"
+//Brown 
+PIXI.game.prodigy.user.chatText.FillColor="#A52A2A"
+//Blue 
+PIXI.game.prodigy.user.chatText.FillColor="#0000FF"
