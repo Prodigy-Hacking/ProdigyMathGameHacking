72335f833a0dc4f686ec17b6fea6656ece8e28d0
Mustan Pathetic <patheticmustan@gmail.com>
2020-03-02 14:28:35 -0500

    Dupe

--- hacks/Currency.js
+++
@@ -1,6 +0,0 @@
-// Gives all currencies.
-PIXI.game.prodigy.player.backpack.data.currency=[]
-x = PIXI.game.state.states.Boot._gameData.currency
-for (i in x) {
-    PIXI.game.prodigy.player.backpack.data.currency[i] = {"ID": x[i].ID, "N": 999999999}
-}
