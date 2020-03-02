e1901ae44e24f02a0f4bc63ad8094dc6dd553c27
ArcerionDev <arcerion114@gmail.com>
2020-03-02 14:18:53 -0500

    Dupe

--- hacks/BecomeAGod.js
+++
@@ -1,4 +0,0 @@
-// Gives you infinite health/ damage.
-PIXI.game.prodigy.player.modifiers.damage=Infinity;
-PIXI.game.prodigy.player.modifiers.maxHearts=Infinity;
-PIXI.game.prodigy.player.heal(Infinity)
