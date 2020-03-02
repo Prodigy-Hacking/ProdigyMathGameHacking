8d1dd717ef7c4733cfb9f29a6be983510aaa26e7
ArcerionDev <arcerion114@gmail.com>
2020-03-02 11:15:17 -0500

    Update Currency.js

--- hacks/Currency.js
+++ hacks/Currency.js
@@ -4,8 +4,3 @@ x = PIXI.game.state.states.Boot._gameData.currency
 for (i in x) {
     PIXI.game.prodigy.player.backpack.data.currency[i] = {"ID": x[i].ID, "N": 999999999}
 }
-//Sets the amount of all your currencies to 9 million.
-x = PIXI.game.prodigy.player.backpack.data.currency
-for (i in x) {
-    x[i] = {"ID": x[i].ID, "N": 9000000}
-}
