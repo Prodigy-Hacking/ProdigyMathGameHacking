b9f1470ac2f23bdd9d7351dfe6caa6ae50622362
Mustan Pathetic <patheticmustan@gmail.com>
2020-03-04 22:27:49 -0500

    eh

---
+++ Cosmetics/CustomChat.js
@@ -0,0 +1,5 @@
+// Partial credit goes to @RubberDuck55, I guess we don't need to hide customchat anymore.
+// This pushes a new message to Prodigy's message filter thingy. Now you can type in this message, and it should show up.
+// Not fully sure if this actually shows up on other computers though....
+
+PIXI.game.prodigy.chat.phrases.push(prompt("What would you like to say?"));
