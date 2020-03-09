3a53af45ccd0673c8bed91de47c8901dbf1c92d0
ArcerionDev <arcerion114@gmail.com>
2020-03-09 09:46:22 -0700

    Update DarkTowerDominator.js

--- setups/DarkTowerDominator.js
+++ setups/DarkTowerDominator.js
@@ -4,5 +4,5 @@ PIXI.game.prodigy.player.modifiers.damage=1000; // 1000x Damage
 PIXI.game.prodigy.player.modifiers.maxHearts=1000; // 1000x HP
 PIXI.game.prodigy.player.heal(9999999999999999999999999999999999999999999999);
 
-//Bookmarklet
+//Bookmarklet:
 javascript:PIXI.game.prodigy.debugMisc.tpTowerFloor(100)%3BPIXI.game.prodigy.player.modifiers.damage%3D1E3%3BPIXI.game.prodigy.player.modifiers.maxHearts%3D1E3%3BPIXI.game.prodigy.player.heal(1E46)%3Bvoid+0
