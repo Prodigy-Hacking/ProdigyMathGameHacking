83ff4e15b7fc89a02da33af11755a72545332411
ArcerionDev <arcerion114@gmail.com>
2020-03-09 08:35:42 -0700

    your welcome

---
+++ setups/Battle GOD.js
@@ -0,0 +1,6 @@
+// Use this script before very battle to DESTROY your enemies.(intended for wizard battles)
+PIXI.game.prodigy.debugMisc.setBattleEnergy(10) //Fills your combat energy!
+PIXI.game.prodigy.debugMisc.easyMode() //You dont have to do math problems anymore!
+PIXI.game.prodigy.player.modifiers.barrier=true //This dosent really help, but makes you look cool whenever you get atacked!
+PIXI.game.prodigy.player.modifiers.maxHearts=1000; //Damage modifiers dont work for wizard battles but health does!
+PIXI.game.prodigy.player.heal(Infinity) //Completly heals your player!
