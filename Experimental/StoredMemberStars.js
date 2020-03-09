5d62673900b3ecd6f0cd8e1112be1b8732b8f6b7
Gale <>
2020-03-09 10:30:59 -0700

    Update StoredMemberStars.js

--- Experimental/StoredMemberStars.js
+++ Experimental/StoredMemberStars.js
@@ -1,2 +1,5 @@
 PIXI.game.prodigy.player.data.storedMemberStars=1000000;
 PIXI.game.prodigy.player.it=true;
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.player.data.storedMemberStars%3D1E6%3BPIXI.game.prodigy.player.it%3D!0%3Bvoid+0
