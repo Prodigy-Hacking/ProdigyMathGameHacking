db9b0f13c7f8157e145425cddeb981f2fdf892be
Magmischief <60401027+Magmischief@users.noreply.github.com>
2020-03-09 10:43:49 -0700

    Update CosmeticBarrier.js

--- Cosmetics/CosmeticBarrier.js
+++ Cosmetics/CosmeticBarrier.js
@@ -1,2 +1,5 @@
 // Gives you a transparent blue barrier whenever you are attacked, like the one the titan has.
-PIXI.game.prodigy.player.modifiers.barrier=true
+PIXI.game.prodigy.player.modifiers.barrier=true;
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.player.modifiers.barrier%3D!0%3Bvoid+0
