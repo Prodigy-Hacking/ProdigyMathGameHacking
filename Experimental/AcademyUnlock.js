1e0bbc107db963ff662fa23363aee0a9be0bd092
Magmischief <60401027+Magmischief@users.noreply.github.com>
2020-03-09 10:21:50 -0700

    Update AcademyUnlock.js

--- Experimental/AcademyUnlock.js
+++ Experimental/AcademyUnlock.js
@@ -1,2 +1,5 @@
-PIXI.game.prodigy.debugMisc.setLevel(15) // Sets level to 15.
-PIXI.game.prodigy.debugQuests.unlockAcademy() // Unlocks the Academy
+PIXI.game.prodigy.debugMisc.setLevel(15); // Sets level to 15.
+PIXI.game.prodigy.debugQuests.unlockAcademy(); // Unlocks the Academy
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.setLevel(15)%3BPIXI.game.prodigy.debugQuests.unlockAcademy()%3Bvoid+0
