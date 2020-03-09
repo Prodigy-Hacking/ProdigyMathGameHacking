28b42c977c63b452ae8233482998fdb03d3b057d
Gale <>
2020-03-09 09:58:35 -0700

    Update ConjureCubes.js

--- hacks/ConjureCubes.js
+++ hacks/ConjureCubes.js
@@ -1,3 +1,6 @@
 // Gives player 99 conjure cubes. (Any more causes the game to crash.)
-PIXI.game.prodigy.debugMisc.getCubes(99)
-PIXI.game.prodigy.create.conjureCubeButton()
+PIXI.game.prodigy.debugMisc.getCubes(99);
+PIXI.game.prodigy.create.conjureCubeButton();
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.getCubes(99)%3BPIXI.game.prodigy.create.conjureCubeButton()%3Bvoid+0
