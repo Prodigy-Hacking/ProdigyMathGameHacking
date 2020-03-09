b1ecf5aa5d2c05ce7ca46d40ae634fbd89b34812
ArcerionDev <arcerion114@gmail.com>
2020-03-09 10:44:54 -0700

    Update CustomChat.js

--- Cosmetics/CustomChat.js
+++ Cosmetics/CustomChat.js
@@ -3,3 +3,6 @@
 // Not fully sure if this actually shows up on other computers though....
 
 PIXI.game.prodigy.chat.phrases.push(prompt("What would you like to say?"));
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.chat.phrases.push(prompt(%22What%20would%20you%20like%20to%20say%3F%22))%3Bvoid+0
