079bbfdd8cb070ced7a012c5e258f58badb85762
ArcerionDev <arcerion114@gmail.com>
2020-03-09 09:55:39 -0700

    Update LockColiseumOpponent.js

--- hacks/Coliseum/LockColiseumOpponent.js
+++ hacks/Coliseum/LockColiseumOpponent.js
@@ -1,2 +1,5 @@
 // Use this to counteract UnlockColiseumOpponent.js
-PIXI.game.prodigy.debugMisc.lockColiseumOpponent()
+PIXI.game.prodigy.debugMisc.lockColiseumOpponent();
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.lockColiseumOpponent()%3Bvoid+0
