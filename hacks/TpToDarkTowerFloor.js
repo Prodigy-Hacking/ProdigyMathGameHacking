06b987d53becb79706321638e9f4119c6a47c1ad
ArcerionDev <arcerion114@gmail.com>
2020-03-09 10:14:11 -0700

    Update TpToDarkTowerFloor.js

--- hacks/TpToDarkTowerFloor.js
+++ hacks/TpToDarkTowerFloor.js
@@ -1,1 +1,4 @@
-PIXI.game.prodigy.debugMisc.tpTowerFloor() // Teleports you to a Dark tower floor of your choice!
+PIXI.game.prodigy.debugMisc.tpTowerFloor(); // Teleports you to a Dark tower floor of your choice!
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.tpTowerFloor()%3Bvoid+0
