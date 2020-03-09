b16e9a66a9b814adbb947fcc11ce1f04bc068784
ArcerionDev <arcerion114@gmail.com>
2020-03-09 09:53:48 -0700

    Update MemberGod.js

--- setups/MemberGod.js
+++ setups/MemberGod.js
@@ -6,5 +6,5 @@ PIXI.game.prodigy.debugMisc.setGameSpeed(10); // 10x walking speed, 100x is anno
 // TODO: Investigate why easy mode breaks battles
 PIXI.game.prodigy.debugMisc.easyMode(1, 100); // Easy mode, 100% correct
 
-//Bookmarklet:
+// Bookmarklet:
 javascript:PIXI.game.prodigy.player.it%3D!0%3BPIXI.game.prodigy.player.modifiers.damage%3D100%3BPIXI.game.prodigy.player.modifiers.maxHearts%3D100%3BPIXI.game.prodigy.debugMisc.setGameSpeed(10)%3BPIXI.game.prodigy.debugMisc.easyMode(1%2C100)%3Bvoid+0
