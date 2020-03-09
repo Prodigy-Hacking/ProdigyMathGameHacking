1867be2447d7e88ca526eba52ab199a5cddbee95
Magmischief <60401027+Magmischief@users.noreply.github.com>
2020-03-09 10:24:07 -0700

    Update CompleteQuest.js

--- Experimental/CompleteQuest.js
+++ Experimental/CompleteQuest.js
@@ -1,2 +1,5 @@
 // Completes quest requirements. For the keywords, go to https://raw.githubusercontent.com/PatheticMustan/ProdigyMathGameHacking/master/Experimental/QuestKeywords.js
-PIXI.game.prodigy.debugQuests.completeQuestRequirements(" ")
+PIXI.game.prodigy.debugQuests.completeQuestRequirements(" ");
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugQuests.completeQuestRequirements(%22%20%22)%3Bvoid+0
