46147b58daeb40cef274cd2b5149269be0c6359b
ArcerionDev <arcerion114@gmail.com>
2020-03-09 10:42:19 -0700

    Update Snow.js

--- DIY/Snow.js
+++ DIY/Snow.js
@@ -1,2 +1,5 @@
 // Creates snow, created/found by @Magmischief.
-PIXI.game.prodigy.debugMisc.snow()
+PIXI.game.prodigy.debugMisc.snow();
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.snow()%3Bvoid+0
