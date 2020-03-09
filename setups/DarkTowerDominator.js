3d0887d81c41c9e73e15a264ac76eb5841040153
ArcerionDev <arcerion114@gmail.com>
2020-03-09 09:45:30 -0700

    bookmarklet

--- setups/DarkTowerDominator.js
+++ setups/DarkTowerDominator.js
@@ -1,5 +1,8 @@
 // Use this right after you get into the Dark Tower!
-PIXI.game.prodigy.debugMisc.tpTowerFloor(100) 
+PIXI.game.prodigy.debugMisc.tpTowerFloor(100);
 PIXI.game.prodigy.player.modifiers.damage=1000; // 1000x Damage
 PIXI.game.prodigy.player.modifiers.maxHearts=1000; // 1000x HP
-PIXI.game.prodigy.player.heal(9999999999999999999999999999999999999999999999)
+PIXI.game.prodigy.player.heal(9999999999999999999999999999999999999999999999);
+
+//Bookmarklet
+javascript:PIXI.game.prodigy.debugMisc.tpTowerFloor(100)%3BPIXI.game.prodigy.player.modifiers.damage%3D1E3%3BPIXI.game.prodigy.player.modifiers.maxHearts%3D1E3%3BPIXI.game.prodigy.player.heal(1E46)%3Bvoid+0
