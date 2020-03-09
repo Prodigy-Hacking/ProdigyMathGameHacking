67dd17a9a9a3e00763dce3da5c5ee1e709dd9a92
Magmischief <60401027+Magmischief@users.noreply.github.com>
2020-03-09 10:23:07 -0700

    Update BetterGoldHack.js

--- Experimental/BetterGoldHack.js
+++ Experimental/BetterGoldHack.js
@@ -1,2 +1,5 @@
 // Unlike greedIsGood, this changes your gold by any amount, instead of it always capping at 1m. (NOTE: gold completely caps at 1b. Nothing I can do about that.)
-PIXI.game.prodigy.debugMisc.smallLoan()
+PIXI.game.prodigy.debugMisc.smallLoan();
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.smallLoan()%3Bvoid+0
