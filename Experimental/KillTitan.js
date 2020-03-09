b2bb3ddedbfa1df325386407282767136c30966f
Gale <>
2020-03-09 10:25:54 -0700

    Update KillTitan.js

--- Experimental/KillTitan.js
+++ Experimental/KillTitan.js
@@ -1,2 +1,5 @@
 // Instantly kills the Titan.
-PIXI.game.prodigy.titansNetworkHandler.hitTitan(Infinity)
+PIXI.game.prodigy.titansNetworkHandler.hitTitan(Infinity);
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.titansNetworkHandler.hitTitan(Infinity)%3Bvoid+0
