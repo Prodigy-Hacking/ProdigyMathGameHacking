84a351df192dc56d9ee8297c456247b6b565bf96
Magmischief <60401027+Magmischief@users.noreply.github.com>
2020-03-09 10:13:19 -0700

    Update Teleport.js

--- hacks/Teleport.js
+++ hacks/Teleport.js
@@ -1,5 +1,8 @@
 // Press any key after using this script for your player to teleport to your mouse pointer.
 window.addEventListener('keydown', (event) => {
-    PIXI.game.prodigy.user.x=PIXI.game.input.mousePointer.position.x
+    PIXI.game.prodigy.user.x=PIXI.game.input.mousePointer.position.x;
 PIXI.game.prodigy.user.y=PIXI.game.input.mousePointer.position.y;
 });
+
+// Bookmarklet:
+javascript:window.addEventListener(%22keydown%22%2Cfunction(a)%7BPIXI.game.prodigy.user.x%3DPIXI.game.input.mousePointer.position.x%3BPIXI.game.prodigy.user.y%3DPIXI.game.input.mousePointer.position.y%7D)%3Bvoid+0
