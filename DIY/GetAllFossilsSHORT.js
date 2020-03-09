c87a88ff4017e5135c33aa98baabac406b365264
ArcerionDev <arcerion114@gmail.com>
2020-03-09 10:36:23 -0700

    Update GetAllFossilsSHORT.js

--- DIY/GetAllFossilsSHORT.js
+++ DIY/GetAllFossilsSHORT.js
@@ -1,2 +1,5 @@
 // Simplified version of getting all fossils. Found by @Magmischief.
-PIXI.game.prodigy.debugMisc.drDino()
+PIXI.game.prodigy.debugMisc.drDino();
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.drDino()%3Bvoid+0
