49cf774a26391118037924570947f642dbe9b5ea
Magmischief <60401027+Magmischief@users.noreply.github.com>
2020-03-09 10:27:12 -0700

    Update PickLimitedWizardSuffixes.js

--- Experimental/PickLimitedWizardSuffixes.js
+++ Experimental/PickLimitedWizardSuffixes.js
@@ -1,2 +1,5 @@
 // Allows you to pick a wizard suffix out of a list Prodigy gives you, rather than just randomly.
-PIXI.game.prodigy.network.open.nameChange()
+PIXI.game.prodigy.network.open.nameChange();
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.network.open.nameChange()%3Bvoid+0
