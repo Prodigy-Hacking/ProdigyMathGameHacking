587efcd68765684a8eabf81c70978a496bf3f643
ArcerionDev <arcerion114@gmail.com>
2020-03-09 10:34:27 -0700

    Update AlwaysCritical.js

--- DIY/AlwaysCritical.js
+++ DIY/AlwaysCritical.js
@@ -1,2 +1,5 @@
 // Always gives you a critical hit. Found by @Magmischief.
-PIXI.game.prodigy.debugMisc.setCritChance(Infinity)
+PIXI.game.prodigy.debugMisc.setCritChance(Infinity);
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.setCritChance(Infinity)%3Bvoid+0
