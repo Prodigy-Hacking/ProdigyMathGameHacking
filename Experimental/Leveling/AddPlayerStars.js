ae523887b928ace73ba4cb112e5ef60fc2707f22
Magmischief <60401027+Magmischief@users.noreply.github.com>
2020-03-09 10:19:55 -0700

    Update AddPlayerStars.js

--- Experimental/Leveling/AddPlayerStars.js
+++ Experimental/Leveling/AddPlayerStars.js
@@ -1,2 +1,5 @@
 // Allows your player to level with the addStars function
-PIXI.game.prodigy.player.addStars()
+PIXI.game.prodigy.player.addStars();
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.player.addStars()%3Bvoid+0
