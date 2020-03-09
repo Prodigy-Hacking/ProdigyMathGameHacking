a6a24efed60018fc2cbf9a30941ab461d6f0e4e5
Magmischief <60401027+Magmischief@users.noreply.github.com>
2020-03-09 10:46:24 -0700

    Update Parade.js

--- Cosmetics/Parade.js
+++ Cosmetics/Parade.js
@@ -1,2 +1,5 @@
 // Makes a parade of monsters come onto the screen!
-PIXI.game.prodigy.debugMisc.parade()
+PIXI.game.prodigy.debugMisc.parade();
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.parade()%3Bvoid+0
