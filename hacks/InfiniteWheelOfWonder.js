8dac59e8f361f96ef96add32a9cd906e3f6dc3bf
Gale <>
2020-03-09 10:09:09 -0700

    Update InfiniteWheelOfWonder.js

--- hacks/InfiniteWheelOfWonder.js
+++ hacks/InfiniteWheelOfWonder.js
@@ -1,2 +1,5 @@
 // Gives you unlimited chances at the Wheel of Wonder.
 PIXI.game.prodigy.player.backpack.data.item.push({"N":Infinity,"ID":130});
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.player.backpack.data.item.push(%7BN%3AInfinity%2CID%3A130%7D)%3Bvoid+0
