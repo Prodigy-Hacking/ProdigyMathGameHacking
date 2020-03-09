5f96dae1af401dc39f2ae74833677e089c7112e4
Magmischief <60401027+Magmischief@users.noreply.github.com>
2020-03-09 09:57:32 -0700

    Update BountyPoints.js

--- hacks/BountyPoints.js
+++ hacks/BountyPoints.js
@@ -1,2 +1,5 @@
 // Gives the player the max amount of bounty points.
-PIXI.game.prodigy.player.data.bountyScore=100
+PIXI.game.prodigy.player.data.bountyScore=100;
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.player.data.bountyScore%3D100%3Bvoid+0
