af32853de5d9a93d2818f5a2612730e709dde0d5
Gale <>
2020-03-09 10:39:54 -0700

    Update SetFPS.js

--- DIY/SetFPS.js
+++ DIY/SetFPS.js
@@ -1,2 +1,5 @@
 // Sets game FPS. Found by @BOOM-BOOM-THE-GREAT.
-PIXI.game.prodigy.debugMisc.SetFPS()
+PIXI.game.prodigy.debugMisc.SetFPS();
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.SetFPS()%3Bvoid+0
