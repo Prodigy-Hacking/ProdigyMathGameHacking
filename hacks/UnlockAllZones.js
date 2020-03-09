a24695f3e284bc44ecf201cec4e3328b02fdc017
ArcerionDev <arcerion114@gmail.com>
2020-03-09 10:15:01 -0700

    Update UnlockAllZones.js

--- hacks/UnlockAllZones.js
+++ hacks/UnlockAllZones.js
@@ -1,2 +1,5 @@
 // Unlocks all zones.
-PIXI.game.prodigy.classModeController.lockedZones=0
+PIXI.game.prodigy.classModeController.lockedZones=0;
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.classModeController.lockedZones%3D0%3Bvoid+0
