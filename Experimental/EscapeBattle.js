3c4a6a2b5097cac7ea975d178e3dc50a984f99b5
Magmischief <60401027+Magmischief@users.noreply.github.com>
2020-03-09 10:24:54 -0700

    Update EscapeBattle.js

--- Experimental/EscapeBattle.js
+++ Experimental/EscapeBattle.js
@@ -1,2 +1,5 @@
 // Use this during combat to instantly escape!
-PIXI.game.prodigy.debugMisc.escapeBattle()
+PIXI.game.prodigy.debugMisc.escapeBattle();
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.escapeBattle()%3Bvoid+0
