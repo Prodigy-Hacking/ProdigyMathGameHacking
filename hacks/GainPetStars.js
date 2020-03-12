56312dc02ed16de8c38ab963574fb631c79dfedd
Arcerion <60945964+Avn1114@users.noreply.github.com>
2020-03-12 11:34:21 -0400

    No.

--- hacks/GainPetStars.js
+++ hacks/GainPetStars.js
@@ -1,8 +1,6 @@
 // Levels all pets to level 100.
-let x = PIXI.game.prodigy.player.kennel.data;
-for (let i=0; i<x.length; i++) { 
-    x.data[i].level=100;
-};
+for (let PetLoopTest = 0; PetLoopTest < PIXI.game.prodigy.player.kennel.data.length; PetLoopTest++) { 
+    PIXI.game.prodigy.player.kennel.data[PetLoopTest].level=100;
 
 // Bookmarklet:
 javascript:for(var%20PetLoopTest%3D0%3BPetLoopTest%3CPIXI.game.prodigy.player.kennel.data.length%3BPetLoopTest%2B%2B)PIXI.game.prodigy.player.kennel.data%5BPetLoopTest%5D.level%3D100%3Bvoid+0
