bfd99b94183d4aec4c21a45a73010395be8ba9d8
Mustan Pathetic <patheticmustan@gmail.com>
2020-03-14 00:10:14 -0400

    Fix

--- hacks/GainPetStars.js
+++ hacks/GainPetStars.js
@@ -1,6 +1,8 @@
 // Levels all pets to level 100.
-for (let PetLoopTest = 0; PetLoopTest < PIXI.game.prodigy.player.kennel.data.length; PetLoopTest++) { 
-    PIXI.game.prodigy.player.kennel.data[PetLoopTest].level=100;
+let x = PIXI.game.prodigy.player.kennel.data;
+for (let i=0; i<x.length; i++) { 
+    x[i].level=100;
+};
 
 // Bookmarklet:
-javascript:for(var%20PetLoopTest%3D0%3BPetLoopTest%3CPIXI.game.prodigy.player.kennel.data.length%3BPetLoopTest%2B%2B)PIXI.game.prodigy.player.kennel.data%5BPetLoopTest%5D.level%3D100%3Bvoid+0
+javascript:(function()%7Blet%20x%20%3D%20PIXI.game.prodigy.player.kennel.data%3Bfor%20(let%20i%3D0%3B%20i%3Cx.length%3B%20i%2B%2B)%20%7Bx%5Bi%5D.level%3D100%3B%7D%7D)()
