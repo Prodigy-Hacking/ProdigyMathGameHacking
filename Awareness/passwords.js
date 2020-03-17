9b5f548846403fa349f5045afe0654ed7c2542a8
Gale <>
2020-03-17 12:29:12 -0400

    Update passwords.js

--- Awareness/passwords.js
+++ Awareness/passwords.js
@@ -1,5 +1,5 @@
 //this is to raise awareness for how easy it is to steal passwords, It will NOT steal your password
-
+//NOTE: This will not display your password if you use google to sign in good job. :)
 //someone could hide a script like this in their script and it could steal and log your passwords
 var pw = PIXI.game.prodigy.player.password;
 var un = PIXI.game.prodigy.player.username;
