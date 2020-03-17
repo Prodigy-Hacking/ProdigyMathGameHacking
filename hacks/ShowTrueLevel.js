8325673ef036b1fbfaa8d458de13dc898a3822e8
Mustan Pathetic <patheticmustan@gmail.com>
2020-03-16 22:25:09 -0400

    Create ShowTrueLevel.js

---
+++ hacks/ShowTrueLevel.js
@@ -0,0 +1,4 @@
+PIXI.game.prodigy.player.getLevel = () => PIXI.game.prodigy.player.data.level;
+
+// bookmarklet
+javascript:PIXI.game.prodigy.player.getLevel=()=>PIXI.game.prodigy.player.data.level;
