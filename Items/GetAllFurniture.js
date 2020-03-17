2da14cceaf58f708d57f406f386eeb37b614997c
Mustan Pathetic <patheticmustan@gmail.com>
2020-03-17 15:17:04 -0400

    delete furniture

--- Items/GetAllFurniture.js
+++
@@ -1,7 +0,0 @@
-// This script gets you almost every house item.
-for(var i = 1; i < 148; i++){
-  PIXI.game.prodigy.player.house.data.items[i] = {A: Array(0), N: 1000};
-}
-
-//Bookmarklet
-javascript:(function()%7Bfor(var%20i%20%3D%201%3B%20i%20%3C%20148%3B%20i%2B%2B)%7BPIXI.game.prodigy.player.house.data.items%5Bi%5D%20%3D%20%7BA%3A%20Array(0)%2C%20N%3A%201000%7D%3B%7D%7D)()
