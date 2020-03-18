1fde7f1949037f090636fba6b44321edb19db9d0
ArcerionDev <arcerion114@gmail.com>
2020-03-18 12:45:52 -0700

    Cheat menu

--- willsCheatMenu/src/utils/util.ts
+++ willsCheatMenu/src/utils/util.ts
@@ -2,4 +2,5 @@ import { GameItemKey } from "../../../typings/gameData";
 import { Item } from "../../../typings/item";
 export const gameData = PIXI.game.state.states.Boot._gameData;
 export const getItem = <T extends GameItemKey>(type: T, id: number): Item<T> | null => gameData[type].find(x => x.ID === id) as null | Item<any> ?? null;
-export const VERY_LARGE_NUMBER = 1e303;
+export const VERY_LARGE_NUMBER = 1e222;
+export const savePlayer = () => PIXI.game.prodigy.player.forceSaveCharacter();
