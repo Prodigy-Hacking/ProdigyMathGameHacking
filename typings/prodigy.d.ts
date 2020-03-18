2c54e360db6be308d75c97da1924402d47ad05b9
Noot <noot>
2020-03-18 11:48:17 -0700

    Cheat menu additions + typings

--- typings/prodigy.d.ts
+++ typings/prodigy.d.ts
@@ -1,8 +1,15 @@
 import { Game } from "./game";
 import { Player } from "./player";
+import { DebugMisc } from "./debugMisc";
+import { TODO } from "./util";
+import { Open } from "./open";
 
 export declare interface Prodigy {
 	version: string;
 	game: Game;
 	player: Player;
+	debugMisc: DebugMisc;
+	world: TODO;
+	giftBoxController: TODO;
+	open: Open;
 }
