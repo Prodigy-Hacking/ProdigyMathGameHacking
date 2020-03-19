c3748410d916214b93cabf32ba90d17fc713846f
Gale <>
2020-03-19 11:49:07 -0700

    i forgot what i did

--- willsCheatMenu/src/utils/keybinds.ts
+++ willsCheatMenu/src/utils/keybinds.ts
@@ -1,21 +1,17 @@
 window.addEventListener("keydown", event => {
-	switch (event.code) {
-		case "KeyL": {
-			PIXI.game.prodigy.user.x = PIXI.game.input.mousePointer.position.x;
-			PIXI.game.prodigy.user.y = PIXI.game.input.mousePointer.position.y;
-			break;
-		}
-		case "KeyE": {
+	if (event.code === "KeyL") {
+		PIXI.game.prodigy.user.x = PIXI.game.input.mousePointer.position.x;
+		PIXI.game.prodigy.user.y = PIXI.game.input.mousePointer.position.y;
+	}
+	if (event.code === "Escape") {
+		PIXI.game.prodigy.open.menus.map(x => x.close());
+	}
+	if (!PIXI.game.prodigy.open.menus.length) {
+		if (event.code === "KeyE") {
 			PIXI.game.prodigy.open.backpack();
-			break;
 		}
-		case "KeyT": {
+		if (event.code === "KeyT") {
 			PIXI.game.prodigy.open.chat();
-			break;
-		}
-		case "Escape": {
-			PIXI.game.prodigy.open.menus.map(x => x.close());
-			break;
 		}
 	}
 });
