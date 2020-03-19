2ef5a1d9ec34c37c0514bb55eda1de1a93d2132b
ArcerionDev <arcerion114@gmail.com>
2020-03-18 20:17:59 -0700

    Location Hacks - Cheat menu

--- willsCheatMenu/src/hacks/location.ts
+++ willsCheatMenu/src/hacks/location.ts
@@ -1,9 +1,54 @@
 import { Hack, category } from "../index";
 import { Swal, Input, Toast } from "../utils/swal";
-import { gameData } from "../utils/util";
-new Hack(
-	category.location,
-	"Teleport To Map (interactive)"
-).setClick(async () => {
-	
-});
+import { gameData, locations } from "../utils/util";
+new Hack(category.location, "Teleport To Map (interactive)").setClick(
+	async () => {
+		const radioPopup = Swal.mixin({
+			focusConfirm: false,
+			showCancelButton: true,
+			preConfirm: () => {
+				return document
+					.querySelector(`.radioDiv[checked]`)
+					?.getAttribute("zone");
+			},
+		});
+		const container = document.createElement("div");
+		container.classList.add("radioContainer");
+		for (const zone of Object.keys(PIXI.game.prodigy.world.zones)) {
+			const radio = document.createElement("DIV");
+			radio.classList.add("radioDiv");
+			radio.setAttribute("zone", zone);
+			const locationURL = locations[zone as keyof typeof locations];
+			if (locationURL)
+				radio.style.backgroundImage = `url(${locationURL})`;
+			else radio.innerText = zone;
+			radio.onclick = () => {
+				document
+					.querySelectorAll(`.radioDiv[checked]`)
+					.forEach(x => x.removeAttribute("checked"));
+				radio.setAttribute("checked", "");
+			};
+			container.append(radio);
+		}
+		const zone = await radioPopup.fire({
+			title: "Teleport Zone",
+			html: container,
+			customClass: {
+				popup: "radioSwal",
+			},
+		});
+		if (!zone.value) return;
+		const mapList = Object.keys(
+			PIXI.game.prodigy.world.zones[zone.value].maps
+		);
+		const area = await Swal.fire({
+			input: "select",
+			inputOptions: Object.fromEntries(mapList.map(x => [x, x])),
+			title: "Map",
+			text: "Which map in the zone do you want to teleport to?"
+		});
+		if (!area.value) return;
+		PIXI.game.prodigy.world.$(`${zone.value}-${area.value}`);
+		await Toast.fire("Teleported", "You have been teleported!", "success")
+	}
+);
