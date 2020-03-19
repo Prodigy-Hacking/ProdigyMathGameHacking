15c522727830ea7f367fb226b0614c2a41f53f9d
Noot <noot>
2020-03-18 18:23:20 -0700

    Locations

--- willsCheatMenu/src/utils/util.ts
+++ willsCheatMenu/src/utils/util.ts
@@ -4,3 +4,6 @@ export const gameData = PIXI.game.state.states.Boot._gameData;
 export const getItem = <T extends GameItemKey>(type: T, id: number): Item<T> | null => gameData[type].find(x => x.ID === id) as null | Item<any> ?? null;
 export const VERY_LARGE_NUMBER = 1e153;
 export const savePlayer = () => PIXI.game.prodigy.player.forceSaveCharacter();
+export const locations = {
+	academy: ""
+}
