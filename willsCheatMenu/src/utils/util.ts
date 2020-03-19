3046bd95e37abd07600b4d06563c565abd0c26eb
Gale <>
2020-03-18 20:17:59 -0700

    Location Hacks - Cheat menu

--- willsCheatMenu/src/utils/util.ts
+++ willsCheatMenu/src/utils/util.ts
@@ -4,6 +4,13 @@ export const gameData = PIXI.game.state.states.Boot._gameData;
 export const getItem = <T extends GameItemKey>(type: T, id: number): Item<T> | null => gameData[type].find(x => x.ID === id) as null | Item<any> ?? null;
 export const VERY_LARGE_NUMBER = 1e153;
 export const savePlayer = () => PIXI.game.prodigy.player.forceSaveCharacter();
+export const assetURL = "https://raw.githubusercontent.com/PatheticMustan/ProdigyMathGameHacking/HEAD/willsCheatMenu/src/assets/"
+export const joinAsset = (asset: string) => `${assetURL}${asset}`;
 export const locations = {
-	academy: ""
+	academy: joinAsset("academy.png"),
+	bonfire_spire: joinAsset("bonfire_spire.png"),
+	forest: joinAsset("forest.png"),
+	shipwreck_shore: joinAsset("shipwreck_shore.png"),
+	shiverchill: joinAsset("shiverchill.png"),
+	skywatch: joinAsset("skywatch.png"),
 }
