7f9a9c31c04549391f3578322f2d5d7d59a6084d
ArcerionDev <arcerion114@gmail.com>
2020-03-22 10:06:42 -0700

    nodejs

---
+++ arenaPoints/src/index.ts
@@ -0,0 +1,90 @@
+import fetch from "node-fetch";
+import data from "../config.json";
+const users: { [index: string]: { token: string; userID: number } } = {};
+(async () => {
+	const fetchJson = async (url: string, opts?: RequestInit | undefined) =>
+		await (await fetch(url, opts as any)).json();
+	if (!data.every(x => x.username && x.password))
+		return console.log("Malformed config.json.");
+	const gameStatus: {
+		data: { gameClientVersion: string };
+	} = await fetchJson("https://api.prodigygame.com/game-api/status");
+	console.log("Game status loaded.");
+	const hack = async (
+		seasonID: number,
+		username: string
+	): Promise<string> => {
+		const user = users[username];
+		if (!user) return "User not found.";
+		const win = await (
+			await fetch(
+				`https://api.prodigygame.com/leaderboard-api/season/${seasonID}/user/${user.userID}/pvp?userID=${user.userID}`,
+				{
+					headers: {
+						authorization: `Bearer ${user.token}`,
+						"content-type":
+							"application/x-www-form-urlencoded; charset=UTF-8",
+					},
+					body: `seasonID=${seasonID}&action=win`,
+					method: "POST",
+				}
+			)
+		).text();
+		if (win === "") return "Failed to increase.";
+		const winJson:
+			| { code: string; points: undefined }
+			| { points: number; code: undefined } = JSON.parse(win);
+		if (winJson.code === "ForbiddenError") {
+			console.log(`[${username} Token Regenerated.`);
+			return hack(seasonID, username);
+		}
+		return `${winJson.points} Points (+100)`;
+	};
+	for (const account of data) {
+		const login = await fetch(
+			"https://api.prodigygame.com/game-auth-api/v1/login",
+			{
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify({
+					username: account.username,
+					password: account.password,
+					clientVersion: gameStatus.data.gameClientVersion,
+				}),
+			}
+		);
+		if (!login.ok) {
+			console.log(
+				`User ${account.username} failed to login: Invalid Credentials.`
+			);
+			break;
+		}
+		const user: { token: string; userID: number } = await login.json();
+		users[account.username] = user;
+		console.log("Logged in.")
+		const lb: { seasonID: number } = await fetchJson(
+			`https://api.prodigygame.com/leaderboard-api/user/${user.userID}/init?userID=${user.userID}`,
+			{
+				headers: {
+					authorization: `Bearer ${user.token}`,
+				},
+			}
+		);
+		console.log("Leaderboard loaded.");
+		console.log(`Hack starting for user ${account.username}.`);
+		const hackify = async () =>
+		console.log(
+			`[${account.username}] ${await hack(
+				lb.seasonID,
+				account.username
+			)}`
+		)
+		hackify()
+		setInterval(
+			hackify,
+			60500
+		);
+	}
+})();
