5d6faf4a400c383fb6ad5cba5f980fff49aaa836
ArcerionDev <arcerion114@gmail.com>
2020-03-25 12:30:11 -0700

    adds

--- willsCheatMenu/src/hacks/misc.ts
+++ willsCheatMenu/src/hacks/misc.ts
@@ -1,4 +1,4 @@
-import { Swal, Toast, NumberInput } from "../utils/swal";
+import { Swal, Toast, NumberInput, Confirm } from "../utils/swal";
 import { Hack, category } from "../index";
 import { VERY_LARGE_NUMBER, gameData, pickRandom } from "../utils/util";
 new Hack(category.misc, "Skip Tutorial").setClick(async () => {
@@ -11,14 +11,47 @@ new Hack(category.misc, "Disable Timeout Dialog").setClick(async () => {
 new Hack(category.misc, "Clothing Vibe").setClick(async () => {
 	setInterval(() => {
 		const rand = <T extends { ID: number }>(arr: T[]) => pickRandom(arr).ID;
-		Phaser.GAMES[0].prodigy.player.equipment.setOutfit(rand(gameData.outfit));
+		Phaser.GAMES[0].prodigy.player.equipment.setOutfit(
+			rand(gameData.outfit)
+		);
 		Phaser.GAMES[0].prodigy.player.equipment.setBoots(rand(gameData.boots));
 		Phaser.GAMES[0].prodigy.player.equipment.setHat(rand(gameData.hat));
 		Phaser.GAMES[0].prodigy.user.reload();
 	}, 1000);
 });
 
-
-new Hack(category.misc, "Bobbify").setClick(async () => {
-
+new Hack(
+	category.misc,
+	"Bobbify",
+	"Converts your account into Bobby Fancywoman."
+).setClick(async () => {
+	if (
+		!(
+			await Confirm.fire(
+				"Are you sure you want your account to be turned into Bobby Fancywoman?",
+				"This action is not reversable."
+			)
+		).value
+	)
+		return;
+	Phaser.GAMES[0].prodigy.debugQuests.completeTutorial();
+	Phaser.GAMES[0].prodigy.player.name.data.nickname = null;
+	Phaser.GAMES[0].prodigy.player.name.data.firstName = 44;
+	Phaser.GAMES[0].prodigy.player.name.data.middleName = 754;
+	Phaser.GAMES[0].prodigy.player.name.data.lastName = 882;
+	Phaser.GAMES[0].prodigy.player.data.stars = -1e22;
+	Phaser.GAMES[0].prodigy.player.data.level = 69;
+	Phaser.GAMES[0].prodigy.player.forceSaveCharacter();
+	Phaser.GAMES[0].prodigy.player.appearance.setGender("male");
+	Phaser.GAMES[0].prodigy.player.appearance.setEyeColor(1);
+	Phaser.GAMES[0].prodigy.player.appearance.setFace(4);
+	Phaser.GAMES[0].prodigy.player.appearance.setHair(19, 1);
+	Phaser.GAMES[0].prodigy.player.appearance.setSkinColor(1);
+	Phaser.GAMES[0].prodigy.player.equipment.setFollow(19);
+	Phaser.GAMES[0].prodigy.player.equipment.setHat(19);
+	Phaser.GAMES[0].prodigy.player.equipment.setBoots(19);
+	Phaser.GAMES[0].prodigy.player.equipment.setOutfit(19);
+	Phaser.GAMES[0].prodigy.player.equipment.setWeapon(19);
+	Phaser.GAMES[0].prodigy.player.forceSaveCharacter();
+	await Toast.fire("Bobbified!", "You are now Bobby Fancywoman.", "success")
 });
