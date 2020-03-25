80313c415517d651b58e3985f96d3eda27be6582
DaChickenKing <61570792+DaChickenKing@users.noreply.github.com>
2020-03-25 12:12:45 -0700

    Update SpamFx.js

--- hacks/SpamFx.js
+++ hacks/SpamFx.js
@@ -6,11 +6,10 @@ setInterval(function(){
 Phaser.GAMES[0].prodigy.network.emitMessage({action:"fx",data:{type:1, userID: Phaser.GAMES[0].prodigy.player.userID}})
 },10);
 
-//Snowball spam. Crashes users:
-setInterval(()=>Phaser.GAMES[0].prodigy.network.emitMessage({"action":"fx","data":{"type":4,"userID":Phaser.GAMES[0].prodigy.player.userID,"x":Math.floor(Math.random() * 1280),"y":Math.floor(Math.random() * 720)}}));
-
-//Water balloon spam. Crashes users:
-setInterval(()=>Phaser.GAMES[0].prodigy.network.emitMessage({"action":"fx","data":{"type":3,"userID":Phaser.GAMES[0].prodigy.player.userID,"x":Math.floor(Math.random() * 1280),"y":Math.floor(Math.random() * 720)}}));
+//Spew fireworks. Doesn't crash users:
+setInterval(function(){
+Phaser.GAMES[0].prodigy.network.emitMessage({action:"fx",data:{type:2, userID: Phaser.GAMES[0].prodigy.player.userID}})
+},10);
 
 
 /*CREDIT*/
