3df7b1b630188488ac65e0f8ed3647544251af5a
Gale <>
2020-03-26 16:33:04 -0700

    cheatmenu

--- willsCheatMenu/src/utils/util.ts
+++ willsCheatMenu/src/utils/util.ts
@@ -1,12 +1,27 @@
 import { GameItemKey } from "../../../typings/gameData";
 import { Item } from "../../../typings/item";
-export const game = Phaser.GAMES[0]
-export const prodigy = game.state.states.Login._gameObj;
+import { Player } from "../../../typings/player";
+import { TODO } from "../../../typings/util";
+export const game = Phaser.GAMES[0];
+export const prodigy: { world: TODO; player: Player } = {
+	player:
+		Phaser.GAMES[0].state.states.Boot._metricsManager
+			.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer
+			._player,
+	world:
+		Phaser.GAMES[0].spriteBatch.renderSession.renderer.game.input.mouseWheel
+			.callbackContext.state.states.Museum._world,
+};
 export const gameData = game.state.states.Boot._gameData;
-export const getItem = <T extends GameItemKey>(type: T, id: number): Item<T> | null => gameData[type].find(x => x.ID === id) as null | Item<any> ?? null;
+export const getItem = <T extends GameItemKey>(
+	type: T,
+	id: number
+): Item<T> | null =>
+	(gameData[type].find(x => x.ID === id) as null | Item<any>) ?? null;
 export const VERY_LARGE_NUMBER = 1e69;
-export const savePlayer = () => game.state.states.Login._gameObj.player.forceSaveCharacter();
-export const assetURL = "https://raw.githubusercontent.com/PatheticMustan/ProdigyMathGameHacking/HEAD/willsCheatMenu/src/assets/"
+export const savePlayer = () => prodigy.player.forceSaveCharacter();
+export const assetURL =
+	"https://raw.githubusercontent.com/PatheticMustan/ProdigyMathGameHacking/HEAD/willsCheatMenu/src/assets/";
 export const joinAsset = (asset: string) => `${assetURL}${asset}`;
 export const locations = {
 	academy: joinAsset("academy.png"),
@@ -15,5 +30,6 @@ export const locations = {
 	shipwreck_shore: joinAsset("shipwreck_shore.png"),
 	shiverchill: joinAsset("shiverchill.png"),
 	skywatch: joinAsset("skywatch.png"),
-}
-export const pickRandom = <T>(arr: T[]): T => arr[Math.floor(Math.random() * arr.length)];
+};
+export const pickRandom = <T>(arr: T[]): T =>
+	arr[Math.floor(Math.random() * arr.length)];
