b36e823584427b4cdfc91d2b664cb376452d6921
Gale <>
2020-03-27 12:05:03 -0700

    player var

--- hacks/InfiniteWheelOfWonder.js
+++ hacks/InfiniteWheelOfWonder.js
@@ -1,5 +1,5 @@
 // Gives you unlimited chances at the Wheel of Wonder.
-Phaser.GAMES[0].state.states.Login._gameObj.player.backpack.data.item.push({"N":Infinity,"ID":130});
+Phaser.CanvasPool.pool[0].parent.game.state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data.item.push({"N":Infinity,"ID":130});
 
 // Bookmarklet:
-// javascript:Phaser.GAMES[0].state.states.Login._gameObj.player.backpack.data.item.push(%7BN%3AInfinity%2CID%3A130%7D)%3Bvoid+0
+// javascript:Phaser.CanvasPool.pool[0].parent.game.state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data.item.push(%7BN%3AInfinity%2CID%3A130%7D)%3Bvoid+0
