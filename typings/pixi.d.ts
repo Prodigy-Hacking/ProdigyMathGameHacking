49beab7115b916822a0efce1a8aea899d475eea0
Gale <>
2020-03-27 10:00:43 -0700

    wills cheat menu fix

--- typings/pixi.d.ts
+++ typings/pixi.d.ts
@@ -1,9 +1,10 @@
 import { Game } from "./game";
 
-export declare interface PIXI {
-}
+export declare interface PIXI {}
 export declare interface Phaser {
-	GAMES: Game[];
+	CanvasPool: {
+		pool: { parent: { game: Game } }[];
+	};
 }
 declare global {
 	const PIXI: PIXI;
