556ed17d9b8f66b2cad837124abfd801c80c8865
Gale <>
2020-04-07 18:31:40 -0400

    EnableDebugStuff

--- hacks/BountyPoints.js
+++ hacks/BountyPoints.js
@@ -1,3 +1,3 @@
-// Gives the player the max amount of bounty points.
+// Gives the player the maximum amount of bounty points.
 (async()=>{e=await(await fetch("https://api.prodigygame.com/game-api/status")).json(),CryptoJS.MD5=(()=>({toString:()=>e.data.prodigyGameFlags.debugPassword})),enableDebug("",!0)})();
 setTimeout(() => {getBounty()}, 100);
