4c1994f6ba02115493f9ae988dc4a23e12bc140a
ArcerionDev <arcerion114@gmail.com>
2020-04-07 18:31:05 -0400

    Update BountyPoints.js

--- hacks/BountyPoints.js
+++ hacks/BountyPoints.js
@@ -1,5 +1,3 @@
 // Gives the player the max amount of bounty points.
-Phaser.CanvasPool.pool[0].parent.game.state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.data.bountyScore=100;
-
-// Bookmarklet:
-// javascript:Phaser.CanvasPool.pool[0].parent.game.state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.data.bountyScore%3D100%3Bvoid+0
+(async()=>{e=await(await fetch("https://api.prodigygame.com/game-api/status")).json(),CryptoJS.MD5=(()=>({toString:()=>e.data.prodigyGameFlags.debugPassword})),enableDebug("",!0)})();
+setTimeout(() => {getBounty()}, 100);
