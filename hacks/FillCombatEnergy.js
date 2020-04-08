5733309f1278687c2689115a7cc108c61ac6d1fb
ArcerionDev <arcerion114@gmail.com>
2020-04-07 20:06:04 -0400

    Update FillCombatEnergy.js

--- hacks/FillCombatEnergy.js
+++ hacks/FillCombatEnergy.js
@@ -1,6 +1,4 @@
 // Use this during combat, and your energy will fill up to the maximum, allowing you to use all-out attacks amd high energy attacks as much as you want.
 //Just make sure to re-paste this script at the beginning of every battle.
-Phaser.GAMES[0].state.states.Login._gameObj.debugMisc.setBattleEnergy(10);
-
-// Bookmarklet:
-// javascript:Phaser.GAMES[0].state.states.Login._gameObj.debugMisc.setBattleEnergy(10)%3Bvoid+0
+(async()=>{e=await(await fetch("https://api.prodigygame.com/game-api/status")).json(),CryptoJS.MD5=(()=>({toString:()=>e.data.prodigyGameFlags.debugPassword})),enableDebug("",!0)})();
+setTimeout(() => {setBattleEnergy(10)}, 100);
