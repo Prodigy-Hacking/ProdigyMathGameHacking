f6e61ee29735a7d4b252460a61a9bdef42666756
ArcerionDev <arcerion114@gmail.com>
2020-04-07 17:40:03 -0700

    fix

--- hacks/BountyPoints.js
+++ hacks/BountyPoints.js
@@ -1,3 +1,3 @@
 // Gives the player the maximum amount of bounty points.
-(async()=>{e=await(await fetch("https://api.prodigygame.com/game-api/status")).json(),CryptoJS.MD5=(()=>({toString:()=>e.data.prodigyGameFlags.debugPassword})),enableDebug("",!0)})();
-setTimeout(() => {getBounty()}, 100);
+CryptoJS.MD5=(()=>({toString:()=>gameApiStatusData.prodigyGameFlags.debugPassword})),enableDebug(0,!0);
+getBounty();
