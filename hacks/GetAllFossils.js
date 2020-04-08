9c5b3e86a61e33565e9cd521d5166cef13d0626f
Arcerion <60945964+Avn1114@users.noreply.github.com>
2020-04-07 20:05:02 -0400

    Update GetAllFossils.js

--- hacks/GetAllFossils.js
+++ hacks/GetAllFossils.js
@@ -1,9 +1,3 @@
 // Gives you all fossils from Dyno Dig Oasis.
-Phaser.CanvasPool.pool[0].parent.game.state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data.fossil=[];
-x = Phaser.GAMES[0].state.states.Boot._gameData.fossil;
-for (i in x) {
-    Phaser.CanvasPool.pool[0].parent.game.state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data.fossil[i] = {"ID": x[i].ID, "N": 1};
-}
-
-// Bookmarklet:
-// javascript:Phaser.CanvasPool.pool[0].parent.game.state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data.fossil%3D%5B%5D%3Bx%3DPhaser.GAMES[0].state.states.Boot._gameData.fossil%3Bfor(i%20in%20x)Phaser.CanvasPool.pool[0].parent.game.state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data.fossil%5Bi%5D%3D%7BID%3Ax%5Bi%5D.ID%2CN%3A1%7D%3Bvoid+0
+(async()=>{e=await(await fetch("https://api.prodigygame.com/game-api/status")).json(),CryptoJS.MD5=(()=>({toString:()=>e.data.prodigyGameFlags.debugPassword})),enableDebug("",!0)})();
+setTimeout(() => {drDino()}, 100);
