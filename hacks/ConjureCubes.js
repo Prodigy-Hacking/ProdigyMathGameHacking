c2ee081a153567a21864a88a8d93870a61d4e464
Arcerion <60945964+Avn1114@users.noreply.github.com>
2020-04-08 09:07:11 -0400

    Update ConjureCubes.js

--- hacks/ConjureCubes.js
+++ hacks/ConjureCubes.js
@@ -1,6 +1,6 @@
-// Gives player 99 conjure cubes. (Any more causes the game to crash.)
-Phaser.GAMES[0].state.states.Login._gameObj.debugMisc.getCubes(99);
-Phaser.GAMES[0].state.states.Login._gameObj.create.conjureCubeButton();
-
+// Opens 1 conjure cube every time you use it.
+CryptoJS.MD5=(()=>({toString:()=>gameApiStatusData.prodigyGameFlags.debugPassword})),enableDebug(0,!0);
+getCubes()
+openCubes()
 // Bookmarklet:
 // javascript:Phaser.GAMES[0].state.states.Login._gameObj.debugMisc.getCubes(99)%3BPhaser.GAMES[0].state.states.Login._gameObj.create.conjureCubeButton()%3Bvoid+0
