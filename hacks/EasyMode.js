4d3ed4a2ada3398e9a251f50984fec2924bb95e9
Arcerion <60945964+Avn1114@users.noreply.github.com>
2020-04-10 10:31:50 -0400

    Not working anymore peoples

--- hacks/EasyMode.js
+++
@@ -1,3 +0,0 @@
-// Makes you not need to answer math questions.
-e=await (await fetch("https://api.prodigygame.com/game-api/status")).json();CryptoJS.MD5 = () => ({ toString: () => e.data.prodigyGameFlags.debugPassword });enableDebug('',!0)
-easyMode()
