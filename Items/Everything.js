016406a6bd9e8f308b2253718788f70d22b62239
Mustan Pathetic <patheticmustan@gmail.com>
2020-04-12 08:50:08 -0400

    Update Everything.js

--- Items/Everything.js
+++ Items/Everything.js
@@ -1,36 +1,32 @@
-//Unlocks all equipment.
-a=["outfit", "hat", "boots", "weapon", "spellRelic", "fossil"];
-for (u of a) {
-   Phaser.GAMES[0].state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data[u]=[];
-    x = Phaser.GAMES[0].state.states.Boot._gameData[u];
-    for (i in x) {
-Phaser.GAMES[0].state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data[u][i] = {"ID": x[i].ID, "N": 1};
+// every time prodigy patches, update these variables to fix the hack.
+const bootData = temp1.object.n.c[0].exports.a.instance.game.state.states.Boot._gameData;
+const backpackData = temp1.object.n.c[0].exports.a.instance.prodigy.player.backpack.data;
+const playerObject = temp1.object.n.c[0].exports.a.instance.prodigy.player;
+
+// all equipment
+let a = ["outfit", "hat", "boots", "weapon", "spellRelic", "fossil", "follow"];
+for (let u of a) {
+    backpackData[u] = [];
+    for (let i in bootData[u]) {
+        backpackData[u][i] = {"ID": bootData[u][i].ID};
     }
 }
 
 // special cases
-Phaser.GAMES[0].state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data.currency=[];
-x = Phaser.GAMES[0].state.states.Boot._gameData.currency;
-for (i in x) {
-    Phaser.GAMES[0].state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data.currency[i] = {"ID": x[i].ID, "N": 99999999};
+// currency
+backpackData.currency = [];
+for (let i in bootData.currency) {
+    backpackData.currency[i] = {"ID": bootData.currency[i].ID, "N": 99999999};
+}
+
+// all items
+backpackData.item=[];
+for (let i in bootData.item) {
+    backpackData.item[i] = {"ID": bootData.item[i].ID, "N": 99999999};
 }
 
 // furniture
-Phaser.GAMES[0].state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.house.data.items = []
-x = Phaser.GAMES[0].state.states.Boot._gameData.dorm
-for (i in x) {
-    Phaser.GAMES[0].state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.house.data.items[x[i]["ID"]] = {A: [], N: 999}
-}
-
-Phaser.GAMES[0].state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data.follow=[];
-x = Phaser.GAMES[0].state.states.Boot._gameData.follow;
-for (i in x) {
-    Phaser.GAMES[0].state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data.follow[i] = {"ID": x[i].ID};
-}
-
-// Gives player all items.
-Phaser.GAMES[0].state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data.item=[];
-x = Phaser.GAMES[0].state.states.Boot._gameData.item;
-for (i in x) {
-    Phaser.GAMES[0].state.states.Boot._metricsManager.gameCompleteDataFactory.gameEventDataBuilder.loggedInPlayer._player.backpack.data.item[i] = {"ID": x[i].ID, "N": 99999999};
+playerObject.house.data.items = []
+for (i in bootData.dorm) {
+    playerObject.house.data.items[gameData.dorm[i]["ID"]] = {A: [], N: 999}
 }
