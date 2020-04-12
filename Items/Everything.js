8633b982178491034c8c077edcb6c61dc4f301e5
Mustan Pathetic <patheticmustan@gmail.com>
2020-04-12 09:05:45 -0400

    Fix everything.js
    Stuck it in an iife so it doesn't collide with stuff in the global scope, that's not good.

--- Items/Everything.js
+++ Items/Everything.js
@@ -1,32 +1,34 @@
-// every time prodigy patches, update these variables to fix the hack.
-const bootData = temp1.object.n.c[0].exports.a.instance.game.state.states.Boot._gameData;
-const backpackData = temp1.object.n.c[0].exports.a.instance.prodigy.player.backpack.data;
-const playerObject = temp1.object.n.c[0].exports.a.instance.prodigy.player;
+(() => {
+    // every time prodigy patches, update these variables to fix the hack.
+    const bootData = temp1.object.n.c[0].exports.a.instance.game.state.states.Boot._gameData;
+    const backpackData = temp1.object.n.c[0].exports.a.instance.prodigy.player.backpack.data;
+    const playerObject = temp1.object.n.c[0].exports.a.instance.prodigy.player;
 
-// all equipment
-let a = ["outfit", "hat", "boots", "weapon", "spellRelic", "fossil", "follow"];
-for (let u of a) {
-    backpackData[u] = [];
-    for (let i in bootData[u]) {
-        backpackData[u][i] = {"ID": bootData[u][i].ID};
+    // all equipment
+    let a = ["outfit", "hat", "boots", "weapon", "spellRelic", "fossil", "follow"];
+    for (let u of a) {
+        backpackData[u] = [];
+        for (let i in bootData[u]) {
+            backpackData[u][i] = {"ID": bootData[u][i].ID};
+        }
     }
-}
 
-// special cases
-// currency
-backpackData.currency = [];
-for (let i in bootData.currency) {
-    backpackData.currency[i] = {"ID": bootData.currency[i].ID, "N": 99999999};
-}
+    // special cases
+    // currency
+    backpackData.currency = [];
+    for (let i in bootData.currency) {
+        backpackData.currency[i] = {"ID": bootData.currency[i].ID, "N": 99999999};
+    }
 
-// all items
-backpackData.item=[];
-for (let i in bootData.item) {
-    backpackData.item[i] = {"ID": bootData.item[i].ID, "N": 99999999};
-}
+    // all items
+    backpackData.item=[];
+    for (let i in bootData.item) {
+        backpackData.item[i] = {"ID": bootData.item[i].ID, "N": 99999999};
+    }
 
-// furniture
-playerObject.house.data.items = []
-for (i in bootData.dorm) {
-    playerObject.house.data.items[gameData.dorm[i]["ID"]] = {A: [], N: 999}
-}
+    // furniture
+    playerObject.house.data.items = []
+    for (i in bootData.dorm) {
+        playerObject.house.data.items[bootData.dorm[i]["ID"]] = {A: [], N: 999}
+    }
+})()
