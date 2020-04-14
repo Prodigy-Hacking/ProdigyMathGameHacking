78545ae24bbd278341ec2415203f72be266c0838
ArcerionDev <arcerion114@gmail.com>
2020-04-14 15:50:02 -0700

    revert

--- UpdatedHacks/maxBounties.js
+++
@@ -1,9 +0,0 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2512
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab560
-\pard\pardeftab560\slleading20\partightenfactor0
-
-\f0\fs24 \cf0 temp1.object.n.c[0].exports.a.instance.prodigy.player.data.bountyScore=100;}
