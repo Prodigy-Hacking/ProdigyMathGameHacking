51bb2a81da2c08a90f88a3153d277953d12427dc
Mustan Pathetic <patheticmustan@gmail.com>
2020-04-14 14:33:39 -0400

    Update Invincibility.js

--- hacks/Invincibility.js
+++ hacks/Invincibility.js
@@ -1,2 +1,2 @@
 // Makes your opponent miss 100% of the time.
-Phaser.GAMES[0].state.states.Login._gameObj.battle.constructor.MOD_DEFAULTS.invincible=true
+temp2.battle.constructor.MOD_DEFAULTS.invincible=true
