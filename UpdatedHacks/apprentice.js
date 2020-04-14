6cbcda7ddae9ae5ea70e8bc7591f5de3da7c3b11
Gale <>
2020-04-14 18:50:09 -0400

    Update apprentice.js

--- UpdatedHacks/apprentice.js
+++ UpdatedHacks/apprentice.js
@@ -1,2 +1,2 @@
 // Sets your name to apprentice (Why you'd need this, I don't really know)
-temp1.object.n.c[0].exports.a.instance.prodigy.player.name.data.firstName=null;}
+temp1.object.n.c[0].exports.a.instance.prodigy.player.name.data.firstName=null;
