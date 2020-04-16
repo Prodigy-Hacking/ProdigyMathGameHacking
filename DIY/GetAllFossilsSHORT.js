afa662cb660fcda819a58bb01af1e15d87320ded
ArcerionDev <arcerion114@gmail.com>
2020-04-16 10:13:07 -0700

    Revert "Massive Rework + Quote Fix"
    This reverts commit 3c0e92dbda73b0fa124548c48cbcc2b175d40fb1.

---
+++ DIY/GetAllFossilsSHORT.js
@@ -0,0 +1,5 @@
+// Simplified version of getting all fossils. Found by @Magmischief.
+Phaser.GAMES[0].state.states.Login._gameObj.debugMisc.drDino();
+
+// Bookmarklet:
+// javascript:Phaser.GAMES[0].state.states.Login._gameObj.debugMisc.drDino()%3Bvoid+0
