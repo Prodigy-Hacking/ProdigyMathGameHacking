fae429ed4fcb3ffad72a10bc35b70c840633b00d
ArcerionDev <arcerion114@gmail.com>
2020-04-16 16:05:07 -0700

    morehacks

--- redirect/global.d.ts
+++ redirect/global.d.ts
@@ -1,6 +1,9 @@
 import { PIXI } from "../typings/pixi";
+import { BackpackData } from "../typings/backpack";
 export declare interface HackFunctions {
 	completeTutorial(): void;
+	getAllPets(): void;
+	getAllItemsInCategory(category: keyof BackpackData): void;
 }
 export declare interface HackVariables {
 	loc: { [index: string]: number; [index: number]: string };
