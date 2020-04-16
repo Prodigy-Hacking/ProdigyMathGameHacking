5ebc94d7c25b4e3f9a6a865fdae18a7ac05238ce
Gale <>
2020-04-16 09:48:47 -0700

    f

--- redirect/index.ts
+++ redirect/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import fetch from "node-fetch";
 const app = express();
+const VERSION = "A-0.0.2"
 app.get("/game.min.js", async(req, res) => {
 	const status: { status: string, data?: { gameClientVersion?: string } } = await (await fetch("https://api.prodigygame.com/game-api/status")).json();
 	const version = status?.data?.gameClientVersion;
@@ -12,7 +13,11 @@ app.get("/game.min.js", async(req, res) => {
 		["t.constants=Object", "hack.constants=t,t.constants=Object"],
 		["window,function(t){var i={};", "window,function(t){var i={};hack.modules=i;"]
 	]
-	return res.send(replacements.reduce((l, c) => l.split(c[0]).join(c[1]) ,`window.hack={};\n${gameMinJS}`))
+	return res.send(replacements.reduce((l, c) => l.split(c[0]).join(c[1]) ,`window.hack={};\n${gameMinJS}
+	console.log("%cWill's Cheat Replacer", "font-size:40px;color:#540052;font-weight:900;font-family:sans-serif;");
+	console.log("%cVersion ${VERSION}", "font-size:20px;color:#000025;font-weight:700;font-family:sans-serif;");
+	console.log('The variable "hack" contains the hacked variables.')
+`))
 })
 app.get("/", (req, res) => res.redirect("/game.min.js"))
 app.get("/public-game.min.js", async(req, res) => {
