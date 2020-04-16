27f797725795cb1d62e9fa60a0dc9df3d828cb63
ArcerionDev <arcerion114@gmail.com>
2020-04-16 08:42:11 -0700

    oh tahts why its crashing

--- redirect/index.ts
+++ redirect/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 import fetch from "node-fetch";
 const app = express();
 app.get("/", async(req, res) => {
-	const status: { status: string, data: { gameClientVersion: string } } = await (await fetch("https://api.prodigygame.com/game-api/status")).json();
-	if (status.status !== "success") return res.sendStatus(503);
-	const gameMinJS = await (await fetch(`https://code.prodigygame.com/code/${status.data.gameClientVersion}/game.min.js?v=${status.data.gameClientVersion}`)).text();
+	const status: { status: string, data?: { gameClientVersion?: string } } = await (await fetch("https://api.prodigygame.com/game-api/status")).json();
+	const version = status?.data?.gameClientVersion;
+	if (status.status !== "success" || !version) return res.sendStatus(503);
+	const gameMinJS = await (await fetch(`https://code.prodigygame.com/code/${version}/game.min.js?v=${version}`)).text();
 	res.type(".js");
 	return res.send(`window.hack={};\n${gameMinJS}`
 		.split("return this._game")
