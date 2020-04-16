3ebcec03f9ed0bc567a6af28addb7858c0564058
Noot <noot>
2020-04-16 09:01:18 -0700

    publicgamemin

--- redirect/index.ts
+++ redirect/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import fetch from "node-fetch";
 const app = express();
-app.get("/", async(req, res) => {
+app.get("/game.min.js", async(req, res) => {
 	const status: { status: string, data?: { gameClientVersion?: string } } = await (await fetch("https://api.prodigygame.com/game-api/status")).json();
 	const version = status?.data?.gameClientVersion;
 	if (status.status !== "success" || !version) return res.sendStatus(503);
@@ -13,5 +13,11 @@ app.get("/", async(req, res) => {
 		.split("t.constants=Object")
 		.join("hack.constants=t,t.constants=Object")
 		)
+})
+app.get("/", (req, res) => res.redirect("/game.min.js"))
+app.get("/public-game.min.js", async(req, res) => {
+	const publicGame = await (await fetch("https://play.prodigygame.com/public/js/public-game.min.js")).text();
+	res.type(".js");
+	return res.send(publicGame.replace(/console\..+?\(.*?\)/g, "(()=>{})()"))
 })
 app.listen(process.env.PORT ?? 1337, () => console.log("Started!"))
