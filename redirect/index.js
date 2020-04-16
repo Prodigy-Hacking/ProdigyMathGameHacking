d9d5785d4d0225cdacc2e8764508788186eb27bc
TNThacker2015 <tnthacker2015@gmail.com>
2020-04-15 20:12:52 -0700

    ff

--- redirect/index.js
+++ redirect/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -34,20 +35,24 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-import express from "express";
-import fetch from "node-fetch";
-var app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var node_fetch_1 = __importDefault(require("node-fetch"));
+var app = express_1.default();
 app.get("/", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
     var status, gameMinJS;
     return __generator(this, function (_a) {
         switch (_a.label) {
-            case 0: return [4 /*yield*/, fetch("https://api.prodigygame.com/game-api/status")];
+            case 0: return [4 /*yield*/, node_fetch_1.default("https://api.prodigygame.com/game-api/status")];
             case 1: return [4 /*yield*/, (_a.sent()).json()];
             case 2:
                 status = _a.sent();
                 if (status.status !== "success")
                     return [2 /*return*/, res.sendStatus(503)];
-                return [4 /*yield*/, fetch("https://code.prodigygame.com/code/" + status.data.gameClientVersion + "/game.min.js?v=" + status.data.gameClientVersion)];
+                return [4 /*yield*/, node_fetch_1.default("https://code.prodigygame.com/code/" + status.data.gameClientVersion + "/game.min.js?v=" + status.data.gameClientVersion)];
             case 3: return [4 /*yield*/, (_a.sent()).text()];
             case 4:
                 gameMinJS = _a.sent();
