177ad26fdac30a6cf747c9679443b3eb94603c53
Arcerion <60945964+Avn1114@users.noreply.github.com>
2020-04-18 18:50:20 -0400

    Update hpOverride.js

--- hacks/Battle/hpOverride.js
+++ hacks/Battle/hpOverride.js
@@ -1,2 +1,2 @@
-// Multiples your hp by 10000000.
-hack.instance.prodigy.player.modifiers.maxHearts=10000000
+// Multiples your hp by 99999999999999.
+hack.instance.prodigy.player.modifiers.maxHearts=99999999999999
