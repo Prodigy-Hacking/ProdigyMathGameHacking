5782651d4332a7ff77f109e79b3ef7c4422f97ef
TNThacker2015 <tnthacker2015@gmail.com>
2020-04-18 15:08:50 -0700

    fix things
    Signed-off-by: TNThacker2015 <tnthacker2015@gmail.com>

--- hacks/appearance/hairColor.js
+++ hacks/appearance/hairColor.js
@@ -1,2 +1,2 @@
 // changes your hair color to a specific color (currently working on a list right now)
-hack.instance.prodigy.player.appearance.hairColor=()
+hack.instance.prodigy.player.appearance.hairColor = 2;
