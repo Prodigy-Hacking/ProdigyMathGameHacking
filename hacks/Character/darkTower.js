0841e275f2a15c634760b47d49e6efe13e66e6a4
Mike <wiikidjj@gmail.com>
2020-04-17 23:10:51 -0400

    fix

--- hacks/Character/darkTower.js
+++ hacks/Character/darkTower.js
@@ -1,6 +1,2 @@
 // makes your Dark Tower Level to 100 
-<<<<<<< HEAD
 hack.instance.prodigy.player.data.tower=100
-=======
-hack.instance.prodigy.player.data.tower=100
->>>>>>> d606819eeffdf56a83325c89ee99d68da40541d2
