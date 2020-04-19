864ec83317c8f8fce9291e0fbb64e9e5d0d9a0da
Mike <62481646+gothboymike@users.noreply.github.com>
2020-04-18 21:26:13 -0400

    Update Hats.js

--- hacks/appearance/Hats.js
+++ hacks/appearance/Hats.js
@@ -1,2 +1,2 @@
-// selects a specific hat
-hack.instance.prodigy.player.equipment.hat=()
+// selects a specific hat also (delete "insertnumberandgetridofthistext" and insert a number there
+hack.instance.prodigy.player.equipment.hat=(insertnumberandgetridofthistext)
