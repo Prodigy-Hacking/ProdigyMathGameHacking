c34ee6809bf7cc6b6088514cb105fd7af1832e87
Gale <>
2020-04-18 21:25:40 -0400

    Update eyeColor.js

--- hacks/appearance/eyeColor.js
+++ hacks/appearance/eyeColor.js
@@ -1,2 +1,2 @@
-// changes your eye color to a specific color (currently working on a list right now)
-hack.instance.prodigy.player.appearance.eyeColor = 2;
+// changes your eye color to a specific color (currently working on a list right now) also (delete "insertnumberandgetridofthistext" and insert a number there
+hack.instance.prodigy.player.appearance.eyeColor=(insertnumberandgetridofthistext)
