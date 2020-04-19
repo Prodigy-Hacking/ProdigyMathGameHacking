227edb66d740f7d66818cd1ba1846ea8a76a0a7e
Gale <>
2020-04-18 21:25:14 -0400

    Update hairColor.js

--- hacks/appearance/hairColor.js
+++ hacks/appearance/hairColor.js
@@ -1,2 +1,2 @@
-// changes your hair color to a specific color (currently working on a list right now)
-hack.instance.prodigy.player.appearance.hairColor = 2;
+// changes your hair color to a specific color (currently working on a list right now) also (delete "insertnumberandgetridofthistext" and insert a number there
+hack.instance.prodigy.player.appearance.hairColor=(insertnumberandgetridofthistext)
