31599a0575cfda63b1d8ac234ab4c407820ad6fe
Mike <62481646+gothboymike@users.noreply.github.com>
2020-04-18 21:22:59 -0400

    Update skinColor.js

--- hacks/appearance/skinColor.js
+++ hacks/appearance/skinColor.js
@@ -1,2 +1,2 @@
-// changes your skintone (prodigy is racist though because white is first)
-hack.instance.prodigy.player.appearance.skinColor = 2;
+// changes your skintone (prodigy is racist though because white is first) also (delete "insertnumberandgetridofthistext" and insert a number there
+hack.instance.prodigy.player.appearance.skinColor=(insertnumberandgetridofthistext)
