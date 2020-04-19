3c0939fc0ceab2c030b0f314274d6fd1728bfa50
Mike <62481646+gothboymike@users.noreply.github.com>
2020-04-18 21:47:00 -0400

    Update volumeSettings.js

--- hacks/Misc/volumeSettings.js
+++ hacks/Misc/volumeSettings.js
@@ -1,5 +1,5 @@
 //> Customizes your volume.
-hack.instance.prodigy.player.data.settings.bgmVolume = 100;
-hack.instance.prodigy.player.data.settings.voiceVolume = 100;
-hack.instance.prodigy.player.data.settings.sfxVolume = 100;
-// Replace 100 with a number.
+hack.instance.prodigy.player.data.settings.bgmVolume = 50;
+hack.instance.prodigy.player.data.settings.voiceVolume = 50;
+hack.instance.prodigy.player.data.settings.sfxVolume = 50;
+// Replace 50 with a number.
