44791126840d767e6b73334031e2971e607bdeee
Mike <62481646+gothboymike@users.noreply.github.com>
2020-04-18 21:22:23 -0400

    Update Outfit.js

--- hacks/appearance/Outfit.js
+++ hacks/appearance/Outfit.js
@@ -1,2 +1,2 @@
-// selects a specific outfit
+// selects a specific outfit (delete "insertnumberandgetridofthistext" and insert a number there
 hack.instance.prodigy.player.equipment.outfit=(insertnumberandgetridofthistext)
