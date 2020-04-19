8d229afb96c7ccb44a373de7a711d3daff8d56ac
ArcerionDev <arcerion114@gmail.com>
2020-04-18 18:29:27 -0700

    2
    Signed-off-by: TNThacker2015 <tnthacker2015@gmail.com>

--- hacks/appearance/hairColor.js
+++ hacks/appearance/hairColor.js
@@ -1,2 +1,2 @@
-// changes your hair color to a specific color (currently working on a list right now) also (delete "insertnumberandgetridofthistext" and insert a number there
-hack.instance.prodigy.player.appearance.hairColor=(insertnumberandgetridofthistext)
+// changes your hair color to a specific color. replace 2 with a number
+hack.instance.prodigy.player.appearance.hairColor= 2
