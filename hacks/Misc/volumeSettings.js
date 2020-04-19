c4936da4bda1a2fa6671e28c74e0db6a3ad736c3
TNThacker2015 <tnthacker2015@gmail.com>
2020-04-19 08:07:58 -0700

    descriptions
    Signed-off-by: TNThacker2015 <tnthacker2015@gmail.com>

--- hacks/Misc/volumeSettings.js
+++ hacks/Misc/volumeSettings.js
@@ -1,4 +1,5 @@
-//> Customizes your volume.
+//> Volume Customizer
+//>> Customizes your volume.
 hack.instance.prodigy.player.data.settings.bgmVolume = 100;
 hack.instance.prodigy.player.data.settings.voiceVolume = 100;
 hack.instance.prodigy.player.data.settings.sfxVolume = 100;
