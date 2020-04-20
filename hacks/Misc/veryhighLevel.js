38e53f488ef43c35d66b70a220805a0ce9aebb85
Mike <62481646+gothboymike@users.noreply.github.com>
2020-04-20 17:00:37 -0400

    update

--- hacks/Misc/veryhighLevel.js
+++ hacks/Misc/veryhighLevel.js
@@ -1,7 +1,3 @@
 //> Level 1e69
 //>> Doesn't save if you logout.
-<<<<<<< HEAD
-hack.instance.prodigy.player.getLevel = () => hack.instance.prodigy.player.data.level = 1e69;
-=======
 hack.instance.prodigy.player.getLevel = () => hack.instance.prodigy.player.data.level = 1e69
->>>>>>> 8846aebf0f42cf31030af326a359b5e4d39c5b2b
