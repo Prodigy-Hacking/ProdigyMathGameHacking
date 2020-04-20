687e52f6f088d287b8f1c891da66c68419bd6c69
Mike <wiikidjj@gmail.com>
2020-04-20 15:28:22 -0400

    update

---
+++ hacks/Battle/bossDifficulty.js
@@ -0,0 +1,16 @@
+// changes boss difficulty to 0 / makes easier ~ credits sailknight ~ founded by gothboymike    
+hack.constants.constants["GameConstants.ReplaceWithBossName.DIFFICULTY"]=0;
+//boss list:
+//Pippet
+//FrostyStorm
+//Gerald
+//Grandoff
+//IceWyrm1
+//IceWyrm2
+//Mira
+//OldOne
+//RainyStorm
+//SandyStorm
+//ShadowGerald
+//StormyStorm
+//Cebollini
