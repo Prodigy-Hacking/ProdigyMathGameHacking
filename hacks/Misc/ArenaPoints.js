9c6883e68755ac58f0c00e985b7c50f327cdf71f
Mustan Pathetic <patheticmustan@gmail.com>
2020-05-01 13:27:21 -0400

    Update ArenaPoints.js

--- hacks/Misc/ArenaPoints.js
+++ hacks/Misc/ArenaPoints.js
@@ -1,19 +1,2 @@
-// Gives you 100 arena points every minute.
-setInterval(async function() {
-await (
-    await fetch(
-        `https://api.prodigygame.com/leaderboard-api/season/${hack.instance.prodigy.gameContainer.get("PVPNetworkHandler").seasonID}/user/${hack.instance.prodigy.player.userID}/pvp?userID=${hack.instance.prodigy.player.userID}`,
-
-        {
-            "headers": {
-                "authorization": `Bearer ${hack.instance.prodigy.gameContainer.get("NetworkManager").jwtAuthProvider.getToken()}`,
-                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                "sec-fetch-mode": "cors"
-            },
-            "referrer": "https://play.prodigygame.com/",
-            "referrerPolicy": "no-referrer-when-downgrade",
-            "body": `seasonID=${hack.instance.prodigy.gameContainer.get("PVPNetworkHandler").seasonID}&action=win`,
-            "method": "POST",
-            "mode": "cors"
-        })
-).text()}, 60 * 1000); 
+// minified
+setInterval(_=>{fetch(`https://api.prodigygame.com/leaderboard-api/season/${hack.instance.prodigy.gameContainer.get("PVPNetworkHandler").seasonID}/user/${hack.instance.prodigy.player.userID}/pvp?userID=${hack.instance.prodigy.player.userID}`,{headers:{authorization:`Bearer ${hack.instance.prodigy.gameContainer.get("NetworkManager").jwtAuthProvider.getToken()}`,"content-type":"application/x-www-form-urlencoded; charset=UTF-8","sec-fetch-mode":"cors"},referrer:"https://play.prodigygame.com/",referrerPolicy:"no-referrer-when-downgrade",body:`seasonID=${hack.instance.prodigy.gameContainer.get("PVPNetworkHandler").seasonID}&action=win`,method:"POST",mode:"cors"}).then(v => console.log(v.text()))},60100);
