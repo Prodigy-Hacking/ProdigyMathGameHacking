4030c5ad070f64555f2da4dc1987bd2a7067d2ad
Divine Lemon <30760966+divinelemon@users.noreply.github.com>
2020-05-11 14:43:51 -0700

    Create flashText.js

---
+++ hacks/Misc/flashText.js
@@ -0,0 +1,5 @@
+//makes it appear once
+hack.instance.prodigy.effects.flashText("TEXT HERE",100,100);
+
+//looped forever
+setInterval(()=>{hack.instance.prodigy.effects.flashText("TEXT HERE",100,100), 1000);})
