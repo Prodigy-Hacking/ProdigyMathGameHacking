203df164a8bbacc0c0be53f475d1ffe634626410
Divine Lemon <30760966+divinelemon@users.noreply.github.com>
2020-05-11 14:40:04 -0700

    Create goldWork.js

---
+++ hacks/Misc/goldWork.js
@@ -0,0 +1,2 @@
+//generates images of gold on the screen, also a few stray fireworks.
+setInterval(()=>{hack.instance.prodigy.effects.goldWork(Math.floor(Math.random() * 1500),Math.floor(Math.random() * 1500),100)})
