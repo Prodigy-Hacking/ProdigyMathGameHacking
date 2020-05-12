4b7aff3685c81b7b8c905c3a582222a85cc5639c
Mike <62481646+gothboymike@users.noreply.github.com>
2020-05-12 15:15:35 -0400

    grammar

--- hacks/info/World.js
+++ hacks/info/World.js
@@ -1,2 +1,2 @@
-// if your too lazy to find what world ur in go to settings to find your world execute this
+// If your too lazy to find what world ur in go to settings to find your world execute this
 hack.instance.prodigy.player.world
