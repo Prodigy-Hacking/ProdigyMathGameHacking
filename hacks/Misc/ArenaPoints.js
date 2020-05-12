85dfb4020b5f0a026a1938f87aefc4f7bba9859d
PatheticMustan <patheticmustan@gmail.com>
2020-05-12 14:39:29 -0400

    ugh

--- hacks/Misc/ArenaPoints.js
+++ hacks/Misc/ArenaPoints.js
@@ -1,2 +1,34 @@
-// minified
-setInterval(_=>{fetch(`https://api.prodigygame.com/leaderboard-api/season/${hack.instance.prodigy.gameContainer.get("PVPNetworkHandler").seasonID}/user/${hack.instance.prodigy.player.userID}/pvp?userID=${hack.instance.prodigy.player.userID}`,{headers:{authorization:`Bearer ${hack.instance.prodigy.gameContainer.get("NetworkManager").jwtAuthProvider.getToken()}`,"content-type":"application/x-www-form-urlencoded; charset=UTF-8","sec-fetch-mode":"cors"},referrer:"https://play.prodigygame.com/",referrerPolicy:"no-referrer-when-downgrade",body:`seasonID=${hack.instance.prodigy.gameContainer.get("PVPNetworkHandler").seasonID}&action=win`,method:"POST",mode:"cors"}).then(v => console.log(v.text()))},60100);
+// unminified
+setInterval(_ => {
+    fetch(
+        (
+            "https://api.prodigygame.com/leaderboard-api/season/" + 
+            hack.instance.prodigy.gameContainer.get("PVPNetworkHandler").seasonID +
+            "/user/" + 
+            hack.instance.prodigy.player.userID + 
+            "/pvp?userID=" + 
+            hack.instance.prodigy.player.userID
+        ),
+        
+        {
+            headers: {
+                "authorization": (
+                    "Bearer " + 
+                    hack.instance.prodigy.gameContainer.get("NetworkManager").jwtAuthProvider.getToken()
+                ),
+                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "sec-fetch-mode": "cors"
+            },
+
+            referrer: "https://play.prodigygame.com/",
+            referrerPolicy: "no-referrer-when-downgrade",
+            body: (
+                "seasonID=" +
+                hack.instance.prodigy.gameContainer.get("PVPNetworkHandler").seasonID + 
+                "&action=win"
+            ),
+            method: "POST",
+            mode: "cors"
+        }
+    ).then(v => console.log(v.text()))
+}, 60100);
