7baca5dfb21021264ff6a4311b9fb8538f8b8efb
PatheticMustan <patheticmustan@gmail.com>
2020-05-12 15:17:58 -0400

    refactor

--- hacks/Misc/fireworks.js
+++
@@ -1,8 +0,0 @@
-// fireworks!
-setInterval(() => {
-    hack.instance.prodigy.effects.firework(
-        Math.floor(Math.random() * 1500),
-        Math.floor(Math.random() * 1500),
-        100
-    );
-});
