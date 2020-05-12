fa0274d5503fdca26cdbdeca7a9e97e2b555e20c
PatheticMustan <patheticmustan@gmail.com>
2020-05-12 15:12:53 -0400

    refactor

--- hacks/Character/completeDinoDig.js
+++
@@ -1,2 +0,0 @@
-// completes dinoDig
-hack.instance.prodigy.game.state.states.DinoDig.complete = true;
--- hacks/character/speed.js
+++ hacks/character/speed.js
@@ -1,3 +1,3 @@
-// literally makes you fast
+// Literally makes you fast
 hack.instance.prodigy.game.state.states.DinoDig.walkSpeed = 100;
-hack.instance.prodigy.user.walkSpeed=10;
+hack.instance.prodigy.user.walkSpeed = 10;
