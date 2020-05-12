79d7eaa78e499875afe7d773d310b00a102b907d
Mustan Pathetic <patheticmustan@gmail.com>
2020-05-12 16:59:19 -0400

    Update inactivityDisabler.js

--- hacks/Misc/inactivityDisabler.js
+++ hacks/Misc/inactivityDisabler.js
@@ -1,2 +1,2 @@
-//makes your screen never ask if you are there, useful when running arena point hack.
-hack.constants.constants["GameConstants.Inactivity.LOG_OUT_TIMER_SECONDS"]=0;
+// No inactivity prompts.
+hack.constants.constants["GameConstants.Inactivity.LOG_OUT_TIMER_SECONDS"] = 0;
