690cb029671a9ac5c06fbf3c34f9ec6f80841931
Mike <62481646+gothboymike@users.noreply.github.com>
2020-05-12 15:29:38 -0400

    update

--- hacks/character/speed.js
+++ hacks/character/speed.js
@@ -1,3 +1,3 @@
-// Literally makes you fast
-hack.instance.prodigy.game.state.states.DinoDig.walkSpeed = 100;
+// Makes you fast
+hack.instance.prodigy.game.state.states.DinoDig.walkSpeed = 100;   ~ (When You Are In The Dino Dig Area Searching For The Items)
 hack.instance.prodigy.user.walkSpeed = 10;
