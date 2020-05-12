436c5bba4acdd22b2506bc3e2392016ce1a06f78
Divine Lemon <30760966+divinelemon@users.noreply.github.com>
2020-05-12 10:54:29 -0700

    Create customName.js (#203)

---
+++ hacks/Character/customName.js
@@ -0,0 +1,2 @@
+//renames your character a name of your choice, does NOT save on reload (unless you are using tampermonkeyHacks).
+hack.instance.prodigy.player.getName = () => hack.instance.prodigy.player.data.name = "TEXT HERE";
