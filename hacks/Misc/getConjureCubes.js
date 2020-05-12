cf40ee321b37a1251dcfe445fef8e3e839d1f049
PatheticMustan <patheticmustan@gmail.com>
2020-05-12 14:45:38 -0400

    lots of refactoring

--- hacks/Misc/getConjureCubes.js
+++ hacks/Misc/getConjureCubes.js
@@ -1,3 +1,8 @@
-//> 99 Conjure Cubes
-//>> Gives you 99 conjure cubes.
-for (let i = 0; i < 100; i++) hack.instance.prodigy.giftBoxController.receiveGiftBox(null, hack.gameData.giftBox.find(x => x.ID === 1));
+// 99 Conjure Cubes
+// Gives you 99 conjure cubes.
+for (let i = 0; i < 100; i++) {
+    hack.instance.prodigy.giftBoxController.receiveGiftBox(
+        null,
+        hack.gameData.giftBox.find(x => x.ID === 1)
+    );
+}
