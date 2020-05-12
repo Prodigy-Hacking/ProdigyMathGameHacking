441d539be606f458277d359f0d37dba7e7059859
Hackintosh-dev <63758321+Hackintosh-dev@users.noreply.github.com>
2020-05-12 14:23:42 -0400

    Update coloredText.js (#204)
    * Update coloredText.js
    
    * Update coloredText.js
    
    Fixed error with other colors saying: SyntaxError: private fields are not currently supported

--- hacks/Misc/coloredText.js
+++ hacks/Misc/coloredText.js
@@ -5,16 +5,30 @@ hack.instance.prodigy.user.chatText.FillColor="#ffffff"
 // Other chat colors:
 
 //Red 
+hack.instance.prodigy.user.chatText.Font="'Comic Sans MS'"
 hack.instance.prodigy.user.chatText.FillColor="#FF0000"
+
 //Silver 
+hack.instance.prodigy.user.chatText.Font="'Comic Sans MS'"
 hack.instance.prodigy.user.chatText.FillColor="#C0C0C0"
+
 //Gray 
-hack.instance.prodigy.user.chatText.FillColor=""#808080"
+hack.instance.prodigy.user.chatText.Font="'Comic Sans MS'"
+hack.instance.prodigy.user.chatText.FillColor="#808080"
+
 //Black 
-hack.instance.prodigy.user.chatText.FillColor=""#000000"
+hack.instance.prodigy.user.chatText.Font="'Comic Sans MS'"
+hack.instance.prodigy.user.chatText.FillColor="#000000"
+
 //Green 
-hack.instance.prodigy.user.chatText.FillColor=""#008000"
+hack.instance.prodigy.user.chatText.Font="'Comic Sans MS'"
+hack.instance.prodigy.user.chatText.FillColor="#008000"
+
 //Brown 
-hack.instance.prodigy.user.chatText.FillColor=""#A52A2A"
+hack.instance.prodigy.user.chatText.Font="'Comic Sans MS'"
+hack.instance.prodigy.user.chatText.FillColor="#A52A2A"
+
 //Blue 
-hack.instance.prodigy.user.chatText.FillColor=""#0000FF"
+hack.instance.prodigy.user.chatText.Font="'Comic Sans MS'"
+hack.instance.prodigy.user.chatText.FillColor="#0000FF"
+
