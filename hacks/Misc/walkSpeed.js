bfcdba5c3c0b3953af1946c65e5b13e45fcedaec
Divine Lemon <30760966+divinelemon@users.noreply.github.com>
2020-05-12 10:52:58 -0700

    Create walkSpeed.js (#202)

---
+++ hacks/Misc/walkSpeed.js
@@ -0,0 +1,2 @@
+//makes you walk very fast. If you set the speed high enough, it may look like teleportation, but monsters will still attack you! If you are trying to evade monsters, use the teleportation hack.
+hack.instance.prodigy.user.walkSpeed=10;
