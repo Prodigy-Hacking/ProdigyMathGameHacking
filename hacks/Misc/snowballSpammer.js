352dfc2ab68c792e5509468e72e835001c3897c2
ArcerionDev <arcerion114@gmail.com>
2020-05-14 20:20:25 -0700

    update to gameContainer.get("NetworkManager") (#212)

--- hacks/Misc/snowballSpammer.js
+++ hacks/Misc/snowballSpammer.js
@@ -2,7 +2,7 @@
 // Spams your surroundings with snowballs.
 for (let i = 0; i < 10000; i++) {
 	setInterval(() =>
-		hack.instance.prodigy.network.emitMessage({
+		hack.instance.prodigy.gameContainer.get("NetworkManager").emitMessage({
 			action: "fx",
 			data: {
 				type: 3,
