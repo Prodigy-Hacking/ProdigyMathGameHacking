61a828f2505b765b4813e148a125367ec02584bd
Mustan Pathetic <patheticmustan@gmail.com>
2020-05-22 12:46:08 -0400

    Fixes #229

--- hacks/Stacker/itemStacker.js
+++ hacks/Stacker/itemStacker.js
@@ -8,8 +8,7 @@
     const bootData = scopeObj.game.state.states.Boot._gameData;
     const backpackData = playerObject.backpack.data;
     
-
-    hack.player.data.gold = 1e69;
+    playerObject.data.gold = playerObject.data.stars = playerObject.data.bountyScore = playerObject.data.level = 1e69;
 
     // all equipment
     let categories = ["boots", "follow", "fossil", "hat", "key", "mathTownFrame", "mathTownInterior", "outfit", "relic", "spellRelic", "weapon"];
@@ -39,4 +38,8 @@
     for (let i in bootData.dorm) {
         playerObject.house.data.items[bootData.dorm[i]["ID"]] = {A: [], N: 999};
     }
+    
+    // Remove bounty notes (#229)
+    let bountyIndex = _ => hack.player.backpack.data.item.findIndex(v => v.ID === 84 || v.ID === 85 || v.ID === 86)
+    while (bountyIndex() > -1) hack.player.backpack.data.item.splice(bountyIndex(), 1)
 })();
