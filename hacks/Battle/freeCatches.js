08dff5ed130b40b83b134567c55defac03da5a90
Divine Lemon <30760966+divinelemon@users.noreply.github.com>
2020-05-24 16:13:53 -0700

    Update freeCatches.js

--- hacks/Battle/freeCatches.js
+++ hacks/Battle/freeCatches.js
@@ -1,2 +1,5 @@
-// Makes your catch cost 0.
+// Makes your catch cost 0 coins.
 hack.constants.constants["GameConstants.Battle.CATCH_COST"] = 0;
+
+//bookmarklet
+javascript:(function()%7Bhack.constants.constants%5B%22GameConstants.Battle.CATCH_COST%22%5D%20%3D%200%3B%7D)()%3B
