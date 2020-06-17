09c303cf114115e503741b661398382e99ee7c04
Mike <62481646+gothboymike@users.noreply.github.com>
2020-06-17 00:23:23 -0400

    yall  have the grammar of a 2nd grader smh

--- hacks/Stacker/itemStacker.js
+++ hacks/Stacker/itemStacker.js
@@ -4,13 +4,13 @@
     const scopeObj = hack.instance;
     const playerObject = hack.player;
     
-    // do not modify.
+    // Do not modify.
     const bootData = scopeObj.game.state.states.Boot._gameData;
     const backpackData = hack.player.backpack.data;
 
     hack.player.data.gold = 1e69;
 
-    // all equipment
+    // All equipment
     let categories = ["boots", "follow", "fossil", "hat", "key", "mathTownFrame", "mathTownInterior", "outfit", "relic", "spellRelic", "weapon"];
     for (let category of categories) {
 		
@@ -21,20 +21,20 @@
         }
     }
 
-    // special cases
-    // currency
+    // Special cases
+    // Currency
     backpackData.currency = [];
     for (let i in bootData.currency) {
         backpackData.currency[i] = {"ID": bootData.currency[i].ID, "N": 99999999};
     }
 
-    // all items
+    // All items
     backpackData.item=[];
     for (let i in bootData.item) {
         backpackData.item[i] = {"ID": bootData.item[i].ID, "N": 99999999};
     }
 
-    // furniture
+    // Furniture
     playerObject.house.data.items = []
     for (let i in bootData.dorm) {
         playerObject.house.data.items[bootData.dorm[i]["ID"]] = {A: [], N: 999};
