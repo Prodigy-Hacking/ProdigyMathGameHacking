79d251f762786953bf2a5cac3621039d2ebc6ea1
Mike <62481646+gothboymike@users.noreply.github.com>
2020-06-21 17:40:06 -0400

    Update ScriptInWork.js

--- Mike's Cheat Menu/ScriptInWork.js
+++ Mike's Cheat Menu/ScriptInWork.js
@@ -10,3 +10,4 @@ div.setAttributeNode(id);
 
 id = document.createAttribute("style");
 id.value = 'dysplay:none; position: absolute; left: 12.7%; z-index: 10; background-color: purple;';
+div.setAttributeNode(id);
