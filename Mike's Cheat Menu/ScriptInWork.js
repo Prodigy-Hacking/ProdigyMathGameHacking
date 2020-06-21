3e100884f0c14723f123ddef912f9f19c4168c5b
ArcerionDev <arcerion114@gmail.com>
2020-06-21 17:47:12 -0400

    Update ScriptInWork.js

--- Mike's Cheat Menu/ScriptInWork.js
+++ Mike's Cheat Menu/ScriptInWork.js
@@ -19,3 +19,4 @@ document.body.insertBefore(div,document.getElementById('game-wrapper'));
 var div1 = document.createElement('div');
 var id = document.createAttribute("id");
 id.value = 'submenu';
+div1.setAttributeNode(id);
