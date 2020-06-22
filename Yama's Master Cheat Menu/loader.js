c7964d146ad2daa208dba422beaca58b53cb5062
ArcerionDev <arcerion114@gmail.com>
2020-06-22 10:13:52 -0400

    Update loader.js

--- Yama's Master Cheat Menu/loader.js
+++ Yama's Master Cheat Menu/loader.js
@@ -1,3 +1,3 @@
-// Run this file in the console to load the cheat menu.
+// Run this file as a bookmarklet to load the cheat menu.
 
-eval((await (await fetch(`https://raw.githubusercontent.com/Prodigy-Hacking/ProdigyMathGameHacking/master/Yama's%20Master%20Cheat%20Menu/Hack%20Menu%20Script%20(Important!)`)).text()));
+javascript:(function()%7Basync%20function%20getMenuData()%7Beval((await%20(await%20fetch(%60https%3A%2F%2Fraw.githubusercontent.com%2FProdigy-Hacking%2FProdigyMathGameHacking%2Fmaster%2FYama's%2520Master%2520Cheat%2520Menu%2FHack%2520Menu%2520Script%2520(Important!)%60)).text()))%7D%0AgetMenuData()%7D)()%3B
