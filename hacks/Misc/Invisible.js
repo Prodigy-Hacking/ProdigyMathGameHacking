07e47cc6629fd93cef518cd726577660742ee78d
Gale <>
2020-07-01 10:49:45 -0700

    Update and rename Invisible.js to NoMonsterBattles.js

--- hacks/Misc/Invisible.js
+++
@@ -1,3 +0,0 @@
-//> Invisible hack
-//>> Allows you to walk past monsters like you're invisible!
-hack.constants.constants["GameConstants.Debug.SCALE_ENCOUNTER_DISTANCE"] = 0;
---
+++ hacks/Misc/NoMonsterBattles.js
@@ -0,0 +1,3 @@
+//> No monster battle hack
+//>> Allows you to walk past monsters and not start battles.
+hack.constants.constants["GameConstants.Debug.SCALE_ENCOUNTER_DISTANCE"] = 0;
