5530b930919e76d683b27b813a1ee824bdf4203d
DaChickenKing <bekfest25@gmail.com>
2020-07-01 10:34:34 -0700

    Update uncapLvl100.js

--- hacks/character/uncapLvl100.js
+++ hacks/character/uncapLvl100.js
@@ -1,2 +1,3 @@
-// Customizes your level to any number you put in here. Doesn't save.
+//> Uncap level hack
+//> Customizes your level to any number you put in. This hack doesn't save.
 hack.player.getLevel = () => hack.player.data.level = numberhere
