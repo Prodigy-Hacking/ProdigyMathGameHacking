7a879569c045cfce5977403ad3b4c005894f0077
Gale <>
2020-07-01 10:49:22 -0700

    Update Invisible.js

--- hacks/Misc/Invisible.js
+++ hacks/Misc/Invisible.js
@@ -1,2 +1,3 @@
-// allows you to walk past monsters like you're invisible!
+//> Invisible hack
+//>> Allows you to walk past monsters like you're invisible!
 hack.constants.constants["GameConstants.Debug.SCALE_ENCOUNTER_DISTANCE"] = 0;
