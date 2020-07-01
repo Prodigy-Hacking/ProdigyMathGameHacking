b8d32490d35026e66fbe9a975d65f6196ac6ba2b
DaChickenKing <bekfest25@gmail.com>
2020-07-01 10:42:21 -0700

    Update teleportation.js

--- hacks/Misc/teleportation.js
+++ hacks/Misc/teleportation.js
@@ -1,4 +1,5 @@
-//makes your character teleport to where your mouse is after 5 seconds. 
+//> Teleportation hack
+//>> makes your character teleport to where your mouse is after 5 seconds. 
 setTimeout(() => {
     hack.instance.prodigy.user.x = hack.instance.prodigy.game.input.mousePointer.position.x;
     hack.instance.prodigy.user.y = hack.instance.prodigy.game.input.mousePointer.position.y;
