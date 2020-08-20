ab80a306f7686175972fd6d98ceab43869eba5f9
Arcerion <60945964+Avn1114@users.noreply.github.com>
2020-08-20 11:01:58 -0400

    :pogchamp:

--- hacks/Misc/ArenaPoints.js
+++ hacks/Misc/ArenaPoints.js
@@ -1,10 +1,12 @@
 //> Arena point hack
 //>> Gives you arena points every minute, DO NOT RUN THIS MORE THAN ONCE, IT WILL RUN ON ITS OWN.
 setInterval(_ => {
+   //> Arena point hack
+//>> Gives you arena points every minute, DO NOT RUN THIS MORE THAN ONCE, IT WILL RUN ON ITS OWN.
    fetch(
         (
             "https://api.prodigygame.com/leaderboard-api/season/" + 
-            hack.instance.prodigy.gameContainer.get("039-53ee").seasonID +
+            hack.instance.prodigy.gameContainer.get("e25-36e7").seasonID +
             "/user/" + 
             hack.player.userID + 
             "/pvp?userID=" + 
@@ -15,7 +17,7 @@ setInterval(_ => {
             headers: {
                 "authorization": (
                     "Bearer " + 
-hack.instance.prodigy.gameContainer.get('896-9b42').jwtAuthProvider.getToken()
+hack.instance.prodigy.gameContainer.get('464-72c6').jwtAuthProvider.getToken()
                 ),
                 "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
                 "sec-fetch-mode": "cors"
@@ -25,7 +27,7 @@ hack.instance.prodigy.gameContainer.get('896-9b42').jwtAuthProvider.getToken()
             referrerPolicy: "no-referrer-when-downgrade",
             body: (
                 "seasonID=" +
-                hack.instance.prodigy.gameContainer.get("039-53ee").seasonID + 
+                hack.instance.prodigy.gameContainer.get("e25-36e7").seasonID + 
                 "&action=win"
             ),
             method: "POST",
