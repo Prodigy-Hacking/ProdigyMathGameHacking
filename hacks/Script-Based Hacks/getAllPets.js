b45a332f5126efe9e6a4b46f99017de5fe875fd3
Gale <>
2020-08-29 22:13:12 -0700

    General cleanup (#288)

--- hacks/Script-Based Hacks/getAllPets.js
+++ hacks/Script-Based Hacks/getAllPets.js
@@ -1,4 +1,4 @@
-function parseJwt (token) {
+function parseJwt(token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
@@ -7,36 +7,39 @@ function parseJwt (token) {
 
     return JSON.parse(jsonPayload);
 };
+
 let userID = parseJwt(localStorage.JWT_TOKEN).content.userID
 let playerdata = await (await fetch(`https://api.prodigygame.com/game-api/v1/character/${userID}?isMember=0&userID=${userID}`, {
     method: 'GET',
     credentials: 'same-origin',
     headers: {
-        'Authorization' : localStorage.JWT_TOKEN,
-        
+        'Authorization': localStorage.JWT_TOKEN,
+
     },
 })).json();
+
 let petdata = await (await fetch('https://cdn.prodigygame.com/game/data/dev/data.json')).json();
 playerdata.pets = []
 for (i = 0; i < petdata.pet.length; i++) {
-let pet = {
-ID : petdata.pet[i].ID,
-catchDate: Date.now(),
-levelCaught: 1,
-Level: 1e+69,
-foreignSpells: [petdata.pet[i].data.foreignSpellPools[0][0],petdata.pet[i].data.foreignSpellPools[1][0]]
+    let pet = {
+        ID: petdata.pet[i].ID,
+        catchDate: Date.now(),
+        levelCaught: 1,
+        Level: 1e+69,
+        foreignSpells: [petdata.pet[i].data.foreignSpellPools[0][0], petdata.pet[i].data.foreignSpellPools[1][0]]
+    }
+    playerdata.pets.push(pet)
 }
-playerdata.pets.push(pet)
-}
+
 await (await fetch(`https://api.prodigygame.com/game-cortex-server/v3/characters/${userID}`, {
     method: 'POST',
     credentials: 'same-origin',
     headers: {
-    "content-type": "application/json",
+        "content-type": "application/json",
         'Authorization': localStorage.JWT_TOKEN,
     },
-body: JSON.stringify({
-            userID: userID,
-data: JSON.stringify(playerdata)
-        }), 
+    body: JSON.stringify({
+        userID: userID,
+        data: JSON.stringify(playerdata)
+    }),
 })).text();
--- hacks/Script-Based Hacks/gold.js
+++ hacks/Script-Based Hacks/gold.js
@@ -1,4 +1,4 @@
-function parseJwt (token) {
+function parseJwt(token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
@@ -7,25 +7,28 @@ function parseJwt (token) {
 
     return JSON.parse(jsonPayload);
 };
+
 let userID = parseJwt(localStorage.JWT_TOKEN).content.userID
 let playerdata = await (await fetch(`https://api.prodigygame.com/game-api/v1/character/${userID}?isMember=0&userID=${userID}`, {
     method: 'GET',
     credentials: 'same-origin',
     headers: {
-        'Authorization' : localStorage.JWT_TOKEN,
-        
+        'Authorization': localStorage.JWT_TOKEN,
+
     },
 })).json();
-playerdata.data.gold = 100000000000
+
+playerdata.data.gold = 100000000000;
+
 await (await fetch(`https://api.prodigygame.com/game-cortex-server/v3/characters/${userID}`, {
     method: 'POST',
     credentials: 'same-origin',
     headers: {
-    "content-type": "application/json",
+        "content-type": "application/json",
         'Authorization': localStorage.JWT_TOKEN,
     },
-body: JSON.stringify({
-            userID: userID,
-data: JSON.stringify(playerdata)
-        }), 
+    body: JSON.stringify({
+        userID: userID,
+        data: JSON.stringify(playerdata)
+    }),
 })).text();
