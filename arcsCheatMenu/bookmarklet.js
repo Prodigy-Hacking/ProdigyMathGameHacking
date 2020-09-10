cc071fa965edb34378106819892fe30dcce37478
Gale <>
2020-09-10 15:21:56 -0400

    f I forgot about gitignore so no node_modules

---
+++ arcsCheatMenu/bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Beval(await%20(await%20fetch('https%3A%2F%2Fraw.githubusercontent.com%2FProdigy-Hacking%2FProdigyMathGameHacking%2Fmaster%2FarcsCheatMenu%2Fdist%2Fbundle.js')).text())%7D)()%3B
