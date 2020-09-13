9ec3fb1768652bd6e0e97e3034816c39b062b5b0
Gale <>
2020-09-12 22:03:45 -0700

    commit

--- updateCommitter.sh
+++ updateCommitter.sh
@@ -1,2 +1,2 @@
-GITHASH=$(git rev-parse @~1922)
+GITHASH=$(git rev-parse @~1832)
 git filter-branch  -f --commit-filter ' if [ $GIT_COMMITTER_NAME == "TNThacker2015" ]; then export GIT_COMMITTER_NAME="noot"; export GIT_COMMITTER_EMAIL="user1@mail.com"; else export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"; export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"; git commit-tree "$@"; fi' -- $GITHASH..HEAD
