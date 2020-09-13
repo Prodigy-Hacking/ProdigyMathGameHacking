27b08438255a5e6998f704c677b3e3c1a68c89de
ArcerionDev <arcerion114@gmail.com>
2020-09-12 22:01:33 -0700

    commit

--- updateCommitter.sh
+++ updateCommitter.sh
@@ -1,3 +1,3 @@
 GITHASH=$(git rev-parse @~1)
-git filter-branch --commit-filter ' if [ $GIT_COMMITTER_NAME == "TNThacker2015" ]; then export GIT_COMMITTER_NAME="noot"; export GIT_COMMITTER_EMAIL="user1@mail.com"; else export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"; export GIT_COMMITTER_DATE="$GIT
-_AUTHOR_DATE"; git commit-tree "$@"; fi' -- $GITHASH..HEAD -f
+git filter-branch  -f --commit-filter ' if [ $GIT_COMMITTER_NAME == "TNThacker2015" ]; then export GIT_COMMITTER_NAME="noot"; export GIT_COMMITTER_EMAIL="user1@mail.com"; else export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"; export GIT_COMMITTER_DATE="$GIT
+_AUTHOR_DATE"; git commit-tree "$@"; fi' -- $GITHASH..HEAD
