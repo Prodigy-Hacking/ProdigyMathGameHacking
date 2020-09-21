a6b322a297925eea3f19ab6291fca78684c6d020
noot <>
2020-09-21 14:44:37 -0700

    funny tokenify script

--- tokenify/index.js
+++ tokenify/index.js
@@ -1,3 +1,79 @@
-import tokenify from "./tokenify.js";
-
-console.log(tokenify("USER", "PASS"))
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.tokenify = void 0;
+const jsdom_1 = __importDefault(require("jsdom"));
+const node_fetch_1 = __importDefault(require("node-fetch"));
+const node_fetch_js_1 = __importDefault(require("fetch-cookie/node-fetch.js"));
+const cookiefetch = node_fetch_js_1.default(node_fetch_1.default);
+const url_1 = require("url");
+exports.tokenify = async (username, password, { log } = {}) => {
+    if (log)
+        console.log("Fetching login route...");
+    const formSite = await cookiefetch("https://sso.prodigygame.com/game/login");
+    if (!formSite.ok)
+        throw new Error(`The form page request was unable to be fetched with a code of ${formSite.status}.`);
+    const site = await formSite.text();
+    const dom = new jsdom_1.default.JSDOM(site);
+    if (log)
+        console.log("Successfully fetched.");
+    const { document } = dom.window;
+    const authenticity = document.querySelector("input[name=authenticity_token]")?.getAttribute("value");
+    if (!authenticity)
+        throw new Error("Authenticity token failed. No authenticity input was found.");
+    if (log)
+        console.log("Authenticity token obtained!");
+    const loginParams = new url_1.URLSearchParams();
+    loginParams.set("utf8", "✓");
+    loginParams.set("authenticity_token", authenticity);
+    loginParams.set("unauthenticated_game_login_form[username]", username);
+    loginParams.set("unauthenticated_game_login_form[password]", password);
+    loginParams.set("button", "");
+    const login = await cookiefetch(formSite.url, {
+        headers: {
+            "content-type": "application/x-www-form-urlencoded",
+        },
+        body: loginParams.toString(),
+        method: "POST",
+        redirect: "manual",
+    });
+    if (!login.ok && !login.status.toString().startsWith("3"))
+        throw new Error(`Initial login request was unsuccessful with code ${login.status}.`);
+    if (log)
+        console.log(`Initial login request done with a code of ${login.status}.`);
+    const playLogin = await cookiefetch(login.headers.get("location") ?? "", { redirect: "follow" });
+    if (!playLogin.ok && !playLogin.status.toString().startsWith("3"))
+        throw new Error(`Client ID request failed with a code of ${playLogin.status}`);
+    if (log)
+        console.log(`Client ID request done with a code of ${playLogin.status}.`);
+    const clientId = (await playLogin.text()).match(/var client_id = '([0-9a-f]+)';/)?.[1];
+    if (clientId === undefined)
+        return console.log("Client ID was not found on in the request response.");
+    const tokenParams = new url_1.URLSearchParams();
+    tokenParams.set("client_id", clientId);
+    tokenParams.set("redirect_uri", "https://play.prodigygame.com/play");
+    tokenParams.set("response_type", "id_token token");
+    tokenParams.set("scope", "openid profile email sid identity_provider");
+    tokenParams.set("state", "b292a37841634f2eb2c6c283285e0e1a");
+    tokenParams.set("nonce", "e651b05312b74195beb22f99a116c630");
+    tokenParams.set("prompt", "login");
+    tokenParams.set("mobilePlatform", "undefined");
+    const tokenLogin = await cookiefetch("https://sso.prodigygame.com/oauth/authorize?" + tokenParams, {
+        redirect: "manual"
+    });
+    if (!tokenLogin.ok && !tokenLogin.status.toString().startsWith("3"))
+        throw new Error(`First authentication request failed with a code of ${tokenLogin.status}.`);
+    if (log)
+        console.log(`First token request done with a code of ${tokenLogin.status}.`);
+    const secondTokenLogin = await cookiefetch(tokenLogin.headers.get("location") ?? "", {
+        redirect: "manual"
+    });
+    if (!secondTokenLogin.ok && !secondTokenLogin.status.toString().startsWith("3"))
+        throw new Error(`Second authentication request failed with a code of ${secondTokenLogin.status}.`);
+    if (log)
+        console.log(`Second token request done with a code of ${secondTokenLogin.status}.`);
+    const tokenProp = new URL((secondTokenLogin.headers.get("location") ?? "").replace("#", "?")).searchParams;
+    return Object.fromEntries(tokenProp.entries());
+};
--- tokenify/index.ts
+++ tokenify/index.ts
@@ -1,46 +1,59 @@
 import JSDOM from "jsdom";
 import fetch from "node-fetch";
+import fetchCookie from "fetch-cookie/node-fetch.js";
+const cookiefetch: (url: RequestInfo, init?: RequestInit | undefined) => Promise<Response> = fetchCookie(fetch) as any;
 import { URLSearchParams } from "url";
-const USERNAME = "redacted";
-const PASSWORD = "raeacted";
-(async () => {
-	console.log("Fetching login route...");
-	const formSite = await fetch("https://sso.prodigygame.com/game/login");
+export const tokenify = async(username: string, password: string, { log }: { log?: boolean } = {}) => {
+	if (log) console.log("Fetching login route...");
+	const formSite: Response = await cookiefetch("https://sso.prodigygame.com/game/login");
+	if (!formSite.ok) throw new Error(`The form page request was unable to be fetched with a code of ${formSite.status}.`);
 	const site = await formSite.text();
-	
 	const dom = new JSDOM.JSDOM(site);
-	console.log("Successfully fetched.");
+	if (log) console.log("Successfully fetched.");
 	const { document } = dom.window;
 	const authenticity = document.querySelector("input[name=authenticity_token]")?.getAttribute("value");
-	if (!authenticity) return console.error("[FATAL] Authenticity token failed.");
-	console.log("Authenticity token obtained!");
+	if (!authenticity) throw new Error("Authenticity token failed. No authenticity input was found.");
+	if (log) console.log("Authenticity token obtained!");
 	const loginParams = new URLSearchParams();
 	loginParams.set("utf8", "✓");
 	loginParams.set("authenticity_token", authenticity);
-	loginParams.set("unauthenticated_game_login_form[username]", USERNAME);
-	loginParams.set("unauthenticated_game_login_form[password]", PASSWORD);
+	loginParams.set("unauthenticated_game_login_form[username]", username);
+	loginParams.set("unauthenticated_game_login_form[password]", password);
 	loginParams.set("button", "");
-	const login = await fetch("https://sso.prodigygame.com/game/login", {
+	const login = await cookiefetch(formSite.url, {
 		headers: {
-			accept:
-				"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-			"accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-CA;q=0.6",
-			"cache-control": "max-age=0",
 			"content-type": "application/x-www-form-urlencoded",
-			"sec-fetch-dest": "document",
-			"sec-fetch-mode": "navigate",
-			"sec-fetch-site": "same-origin",
-			"sec-fetch-user": "?1",
-			"upgrade-insecure-requests": "1",
-			"cookie": formSite.headers.get("set-cookie")!
 		},
 		body: loginParams.toString(),
 		method: "POST",
+		redirect: "manual",
+	});
+	if (!login.ok && !login.status.toString().startsWith("3")) throw new Error(`Initial login request was unsuccessful with code ${login.status}.`);
+	if (log) console.log(`Initial login request done with a code of ${login.status}.`);
+	const playLogin = await cookiefetch(login.headers.get("location") ?? "", { redirect: "follow" });
+	if (!playLogin.ok && !playLogin.status.toString().startsWith("3")) throw new Error(`Client ID request failed with a code of ${playLogin.status}`);
+	if (log) console.log(`Client ID request done with a code of ${playLogin.status}.`);
+	const clientId = (await playLogin.text()).match(/var client_id = '([0-9a-f]+)';/)?.[1];
+	if (clientId === undefined) return console.log("Client ID was not found on in the request response.");
+	const tokenParams = new URLSearchParams();
+	tokenParams.set("client_id", clientId);
+	tokenParams.set("redirect_uri", "https://play.prodigygame.com/play");
+	tokenParams.set("response_type", "id_token token");
+	tokenParams.set("scope", "openid profile email sid identity_provider");
+	tokenParams.set("state", "b292a37841634f2eb2c6c283285e0e1a");
+	tokenParams.set("nonce", "e651b05312b74195beb22f99a116c630");
+	tokenParams.set("prompt", "login");
+	tokenParams.set("mobilePlatform", "undefined");
+	const tokenLogin = await cookiefetch("https://sso.prodigygame.com/oauth/authorize?" + tokenParams, {
 		redirect: "manual"
 	});
-	// if (!login.ok) console.error("Failed to login.")
-	console.log(`Logged in with status ${login.status}.`);
-	console.log(await login.text())
-	console.log(Object.fromEntries(login.headers.entries()))
-	console.log(login.redirected)
-})();
+	if (!tokenLogin.ok && !tokenLogin.status.toString().startsWith("3")) throw new Error(`First authentication request failed with a code of ${tokenLogin.status}.`)
+	if (log) console.log(`First token request done with a code of ${tokenLogin.status}.`);
+	const secondTokenLogin = await cookiefetch(tokenLogin.headers.get("location") ?? "", {
+		redirect: "manual"
+	});
+	if (!secondTokenLogin.ok && !secondTokenLogin.status.toString().startsWith("3")) throw new Error(`Second authentication request failed with a code of ${secondTokenLogin.status}.`)
+	if (log) console.log(`Second token request done with a code of ${secondTokenLogin.status}.`);
+	const tokenProp = new URL((secondTokenLogin.headers.get("location") ?? "").replace("#", "?")).searchParams;
+	return Object.fromEntries(tokenProp.entries());
+};
