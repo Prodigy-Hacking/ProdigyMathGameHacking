12ea3423e31d3910a1f7e012470f75dfa88a57fb
Mustan Pathetic <patheticmustan@gmail.com>
2020-09-29 13:05:22 -0400

    #169 (#311)

--- hacks/uselessScripts/membership.js
+++
@@ -1,4 +0,0 @@
-//> Membership
-//>> This script will give you non-permanent membership. It doesn't do anything as our redirector hack already gives you this automatically.
-
-_.instance.prodigy.game.P = true;
