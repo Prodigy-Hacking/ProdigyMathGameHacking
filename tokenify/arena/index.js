ee0c033cfe2d6c4f462abc0f6a6f59dd318ad901
Gale <>
2020-10-01 16:18:10 -0700

    threads

--- tokenify/arena/index.js
+++ tokenify/arena/index.js
@@ -6,76 +6,106 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const node_fetch_1 = __importDefault(require("node-fetch"));
 const config_json_1 = __importDefault(require("./config.json"));
 const tokenify_1 = require("../../tokenify/");
+const worker_threads_1 = require("worker_threads");
+const fetchJson = async (url, opts) => await (await node_fetch_1.default(url, opts)).json();
+const users = {};
 const main = async () => {
     foo: for (const _ in [2]) {
-        const users = {};
-        const fetchJson = async (url, opts) => await (await node_fetch_1.default(url, opts)).json();
         if (!config_json_1.default.every(x => x.username && x.password))
             return console.log("Malformed config.json.");
         const gameStatus = await fetchJson("https://api.prodigygame.com/game-api/status");
         console.log("Game status loaded.");
-        const hack = async (seasonID, username, password) => {
-            const user = users[username];
-            if (!user)
-                return "User not found.";
-            const win = await (await node_fetch_1.default(`https://api.prodigygame.com/leaderboard-api/season/${seasonID}/user/${user.userID}/pvp?userID=${user.userID}`, {
-                headers: {
-                    authorization: `Bearer ${user.token}`,
-                    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                    'user-agent': 'CUM MACHINE / v1.12.2',
-                },
-                body: `seasonID=${seasonID}&action=win`,
-                method: "POST",
-            })).text();
-            if (win === "")
-                return "Failed to increase.";
-            const winJson = JSON.parse(win);
-            if (winJson.code === "ForbiddenError") {
-                process.exit(1);
-            }
-            if (winJson.code === "TooManyRequests")
-                return "Ratelimited.";
-            if (winJson.points === undefined)
-                return JSON.stringify(winJson);
-            const rank = await fetchJson(`https://api.prodigygame.com/leaderboard-api/season/${seasonID}/user/${user.userID}/rank?userID=${user.userID}`, {
-                headers: {
-                    authorization: `Bearer ${user.token}`,
-                    'user-agent': 'CUM MACHINE / v1.12.2',
+        const chunk = (arr, size) => arr.reduce((acc, _, i) => {
+            if (i % size === 0)
+                acc.push(arr.slice(i, i + size));
+            return acc;
+        }, []);
+        for (const acc of chunk(config_json_1.default, 50)) {
+            const worker = new worker_threads_1.Worker(__filename, {
+                workerData: {
+                    accounts: acc,
+                    gameStatus,
                 },
             });
-            return `${`${winJson.points} Points (+100)`.padEnd(20)} - Rank: ${rank.rank}`;
-        };
-        await Promise.all(config_json_1.default.map(async (account) => {
-            const login = await node_fetch_1.default("https://api.prodigygame.com/game-auth-api/v1/login", {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json",
-                },
-                body: JSON.stringify({
-                    username: account.username,
-                    password: account.password,
-                    clientVersion: gameStatus.data.gameClientVersion,
-                }),
+            worker.on("message", m => console.log(`[${String(worker.threadId).padStart(2, "0")}] ${m}`));
+            worker.on("error", r => console.error(r));
+            worker.on("exit", code => {
+                if (code !== 0)
+                    console.error(`Worker ${worker.threadId} stopped with exit code ${code}`);
             });
-            const user = await tokenify_1.tokenify(account.username, account.password);
-            users[account.username] = user;
-            console.log("Logged in.");
-            const lb = await fetchJson(`https://api.prodigygame.com/leaderboard-api/user/${user.userID}/init?userID=${user.userID}`, {
-                headers: {
-                    authorization: `Bearer ${user.token}`,
-                },
-            });
-            console.log("Leaderboard loaded.");
-            const char = await fetchJson(`https://api.prodigygame.com/game-api/v2/characters/${user.userID}`, {
-                headers: {
-                    authorization: `Bearer ${user.token}`,
-                },
-            });
-            console.log(`Hack starting for user ${account.username}.`);
-            const hackify = async () => console.log(`${`[${account.username}]`.padEnd(22)} ${await hack(lb.seasonID, account.username, account.password)}`);
-            hackify();
-            setInterval(hackify, 60500);
-        }));
+        }
     }
 };
-main();
+const doThread = async () => {
+    if (!worker_threads_1.parentPort)
+        return;
+    const hack = async (seasonID, username, password) => {
+        const user = users[username];
+        if (!user)
+            return "User not found.";
+        const win = await (await node_fetch_1.default(`https://api.prodigygame.com/leaderboard-api/season/${seasonID}/user/${user.userID}/pvp?userID=${user.userID}`, {
+            headers: {
+                authorization: `Bearer ${user.token}`,
+                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "user-agent": "CUM MACHINE / v1.12.2",
+            },
+            body: `seasonID=${seasonID}&action=win`,
+            method: "POST",
+        })).text();
+        if (win === "")
+            return "Failed to increase.";
+        const winJson = JSON.parse(win);
+        if (winJson.code === "ForbiddenError") {
+            process.exit(1);
+        }
+        if (winJson.code === "TooManyRequests")
+            return "Ratelimited.";
+        if (winJson.points === undefined)
+            return JSON.stringify(winJson);
+        const rank = await fetchJson(`https://api.prodigygame.com/leaderboard-api/season/${seasonID}/user/${user.userID}/rank?userID=${user.userID}`, {
+            headers: {
+                authorization: `Bearer ${user.token}`,
+                "user-agent": "CUM MACHINE / v1.12.2",
+            },
+        });
+        return `${`${winJson.points} Points (+100)`.padEnd(20)} - Rank: ${rank.rank}`;
+    };
+    const acc = worker_threads_1.workerData.accounts;
+    const gameStatus = worker_threads_1.workerData.gameStatus;
+    for (const account of acc) {
+        const login = await node_fetch_1.default("https://api.prodigygame.com/game-auth-api/v1/login", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+                username: account.username,
+                password: account.password,
+                clientVersion: gameStatus.data.gameClientVersion,
+            }),
+        });
+        const user = await tokenify_1.tokenify(account.username, account.password);
+        users[account.username] = user;
+        worker_threads_1.parentPort.postMessage("Logged in.");
+        const lb = await fetchJson(`https://api.prodigygame.com/leaderboard-api/user/${user.userID}/init?userID=${user.userID}`, {
+            headers: {
+                authorization: `Bearer ${user.token}`,
+            },
+        });
+        worker_threads_1.parentPort.postMessage("Leaderboard loaded.");
+        const char = await fetchJson(`https://api.prodigygame.com/game-api/v2/characters/${user.userID}`, {
+            headers: {
+                authorization: `Bearer ${user.token}`,
+            },
+        });
+        worker_threads_1.parentPort.postMessage(`Hack starting for user ${account.username}.`);
+        const hackify = async () => worker_threads_1.parentPort?.postMessage(`${`[${account.username}]`.padEnd(22)} ${await hack(lb.seasonID, account.username, account.password)}`);
+        hackify();
+        setInterval(hackify, 60500);
+    }
+};
+if (worker_threads_1.isMainThread)
+    main();
+else {
+    doThread();
+}
--- tokenify/arena/index.ts
+++ tokenify/arena/index.ts
@@ -1,117 +1,122 @@
 import fetch from "node-fetch";
 import data from "./config.json";
 import chalk from "chalk";
-import { tokenify, renewToken } from "../../tokenify/"
+import { tokenify, renewToken } from "../../tokenify/";
 import { RequestInit } from "node-fetch";
 import proxyAgent from "https-proxy-agent";
+import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
+const fetchJson = async (url: string, opts?: RequestInit | undefined) => await (await fetch(url, opts as any)).json();
+const users: {
+	[index: string]: { token: string; userID: number };
+} = {};
 const main = async () => {
-	foo:
-	for (const _ in [2]) {
-		const users: {
-			[index: string]: { token: string; userID: number };
-		} = {};
-		const fetchJson = async (url: string, opts?: RequestInit | undefined) =>
-			await (await fetch(url, opts as any)).json();
-		if (!data.every(x => x.username && x.password))
-			return console.log("Malformed config.json.");
+	foo: for (const _ in [2]) {
+		if (!data.every(x => x.username && x.password)) return console.log("Malformed config.json.");
 		const gameStatus: {
 			data: { gameClientVersion: string };
 		} = await fetchJson("https://api.prodigygame.com/game-api/status");
 		console.log("Game status loaded.");
-		const hack = async (
-			seasonID: number,
-			username: string,
-			password: string
-		): Promise<string> => {
-			const user = users[username];
-			if (!user) return "User not found.";
-			const win = await (
-				await fetch(
-					`https://api.prodigygame.com/leaderboard-api/season/${seasonID}/user/${user.userID}/pvp?userID=${user.userID}`,
-					{
-						headers: {
-							authorization: `Bearer ${user.token}`,
-							"content-type":
-								"application/x-www-form-urlencoded; charset=UTF-8",
-							'user-agent': 'CUM MACHINE / v1.12.2',
-						},
-						body: `seasonID=${seasonID}&action=win`,
-						method: "POST",
-					}
-				)
-			).text();
-			if (win === "") return "Failed to increase.";
-			const winJson:
-				| { code: string; points: undefined }
-				| { points: number; code: undefined } = JSON.parse(win);
-			if (winJson.code === "ForbiddenError") {
-				process.exit(1);
-			}
-			if (winJson.code === "TooManyRequests") return "Ratelimited."
-			if (winJson.points === undefined) return JSON.stringify(winJson);
-			const rank: { rank: number } = await fetchJson(
-				`https://api.prodigygame.com/leaderboard-api/season/${seasonID}/user/${user.userID}/rank?userID=${user.userID}`,
-				{
-					headers: {
-						authorization: `Bearer ${user.token}`,
-						'user-agent': 'CUM MACHINE / v1.12.2',
-					},
-				}
-			);
-			return `${`${winJson.points} Points (+100)`.padEnd(20)} - Rank: ${
-				rank.rank
-			}`;
-		};
-		await Promise.all(data.map(async account => {
-			const login = await fetch(
-				"https://api.prodigygame.com/game-auth-api/v1/login",
-				{
-					method: "POST",
-					headers: {
-						"Content-type": "application/json",
-					},
-					body: JSON.stringify({
-						username: account.username,
-						password: account.password,
-						clientVersion: gameStatus.data.gameClientVersion,
-					}),
-				}
-			);
-			const user: {
-				token: string;
-				userID: number;
-			} = await tokenify(account.username, account.password);
-			users[account.username] = user;
-			console.log("Logged in.");
-			const lb: { seasonID: number } = await fetchJson(
-				`https://api.prodigygame.com/leaderboard-api/user/${user.userID}/init?userID=${user.userID}`,
-				{
-					headers: {
-						authorization: `Bearer ${user.token}`,
-					},
-				}
-			);
-			console.log("Leaderboard loaded.");
-			const char = await fetchJson(
-				`https://api.prodigygame.com/game-api/v2/characters/${user.userID}`,
-				{
-					headers: {
-						authorization: `Bearer ${user.token}`,
-					},
-				}
-			);
-			console.log(`Hack starting for user ${account.username}.`);
-			const hackify = async () =>
-				console.log(
-					`${`[${account.username}]`.padEnd(22)} ${await hack(
-						lb.seasonID,
-						account.username,
-						account.password
-					)}`
-				);
-			hackify();
-			setInterval(hackify, 60500);
-		}));
+		const chunk = <T>(arr: T[], size: number): T[][] =>
+			arr.reduce((acc: T[][], _, i) => {
+				if (i % size === 0) acc.push(arr.slice(i, i + size));
+				return acc;
+			}, []);
+		for (const acc of chunk(data, 30)) {
+			const worker = new Worker(__filename, {
+				workerData: {
+					accounts: acc,
+					gameStatus,
+				},
+			});
+			worker.on("message", m => console.log(`[${String(worker.threadId).padStart(2, "0")}] ${m}`));
+			worker.on("error", r => console.error(r));
+			worker.on("exit", code => {
+				if (code !== 0) console.error(`Worker ${worker.threadId} stopped with exit code ${code}`);
+			});
+		}
 	}
 };
-main();
+const doThread = async () => {
+	if (!parentPort) return;
+	const hack = async (seasonID: number, username: string, password: string): Promise<string> => {
+		const user = users[username];
+		if (!user) return "User not found.";
+		const win = await (
+			await fetch(
+				`https://api.prodigygame.com/leaderboard-api/season/${seasonID}/user/${user.userID}/pvp?userID=${user.userID}`,
+				{
+					headers: {
+						authorization: `Bearer ${user.token}`,
+						"content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+						"user-agent": "CUM MACHINE / v1.12.2",
+					},
+					body: `seasonID=${seasonID}&action=win`,
+					method: "POST",
+				}
+			)
+		).text();
+		if (win === "") return "Failed to increase.";
+		const winJson: { code: string; points: undefined } | { points: number; code: undefined } = JSON.parse(win);
+		if (winJson.code === "ForbiddenError") {
+			process.exit(1);
+		}
+		if (winJson.code === "TooManyRequests") return "Ratelimited.";
+		if (winJson.points === undefined) return JSON.stringify(winJson);
+		const rank: { rank: number } = await fetchJson(
+			`https://api.prodigygame.com/leaderboard-api/season/${seasonID}/user/${user.userID}/rank?userID=${user.userID}`,
+			{
+				headers: {
+					authorization: `Bearer ${user.token}`,
+					"user-agent": "CUM MACHINE / v1.12.2",
+				},
+			}
+		);
+		return `${`${winJson.points} Points (+100)`.padEnd(20)} - Rank: ${rank.rank}`;
+	};
+	const acc: { username: string; password: string }[] = workerData.accounts;
+	const gameStatus = workerData.gameStatus;
+	for (const account of acc) {
+		const login = await fetch("https://api.prodigygame.com/game-auth-api/v1/login", {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify({
+				username: account.username,
+				password: account.password,
+				clientVersion: gameStatus.data.gameClientVersion,
+			}),
+		});
+		const user: {
+			token: string;
+			userID: number;
+		} = await tokenify(account.username, account.password);
+		users[account.username] = user;
+		parentPort.postMessage("Logged in.");
+		const lb: { seasonID: number } = await fetchJson(
+			`https://api.prodigygame.com/leaderboard-api/user/${user.userID}/init?userID=${user.userID}`,
+			{
+				headers: {
+					authorization: `Bearer ${user.token}`,
+				},
+			}
+		);
+		parentPort.postMessage("Leaderboard loaded.");
+		const char = await fetchJson(`https://api.prodigygame.com/game-api/v2/characters/${user.userID}`, {
+			headers: {
+				authorization: `Bearer ${user.token}`,
+			},
+		});
+		parentPort.postMessage(`Hack starting for user ${account.username}.`);
+		const hackify = async () =>
+			parentPort?.postMessage(
+				`${`[${account.username}]`.padEnd(22)} ${await hack(lb.seasonID, account.username, account.password)}`
+			);
+		hackify();
+		setInterval(hackify, 60500);
+	}
+};
+if (isMainThread) main();
+else {
+	doThread();
+}
