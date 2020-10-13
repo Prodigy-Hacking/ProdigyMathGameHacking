66eb834f529a2907cd31b8724547a404a34f7ea4
Gale <>
2020-10-13 16:21:37 -0400

    fix arenapoints lol

--- hacks/misc/arenaPoints.js
+++ hacks/misc/arenaPoints.js
@@ -1,39 +1,15 @@
-//> Arena point hack
-//>> Gives you arena points every minute, DO NOT RUN THIS MORE THAN ONCE, IT WILL RUN ON ITS OWN.
 setInterval(_ => {
-   //> Arena point hack
-//>> Gives you arena points every minute, DO NOT RUN THIS MORE THAN ONCE, IT WILL RUN ON ITS OWN.
-   fetch(
-        (
-            "https://api.prodigygame.com/leaderboard-api/season/" + 
-            _.instance.prodigy.gameContainer.get("e25-36e7").seasonID +
-            "/user/" + 
-            _.player.userID + 
-            "/pvp?userID=" + 
-            _.player.userID
-        ),
-        
-        {
+    fetch(
+        ("https://api.prodigygame.com/leaderboard-api/season/" + _.instance.prodigy.gameContainer.get("293-6cb0").seasonID + "/user/" + _.player.userID + "/pvp?userID=" + _.player.userID), {
             headers: {
-                "authorization": (
-                    "Bearer " + 
-_.instance.prodigy.gameContainer.get('464-72c6').jwtAuthProvider.getToken()
-                ),
+                "authorization": ("Bearer " + _.network.jwtAuthProvider.getToken()),
                 "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
                 "sec-fetch-mode": "cors"
             },
-
             referrer: "https://play.prodigygame.com/",
             referrerPolicy: "no-referrer-when-downgrade",
-            body: (
-                "seasonID=" +
-                _.instance.prodigy.gameContainer.get("e25-36e7").seasonID + 
-                "&action=win"
-            ),
+            body: ("seasonID=" + _.instance.prodigy.gameContainer.get("293-6cb0").seasonID + "&action=win"),
             method: "POST",
             mode: "cors"
-        }
-    )
-    .then(v => v.text())
-    .then(v => console.log(v))
+        }).then(v => v.text()).then(v => console.log(v))
 }, 60500);
