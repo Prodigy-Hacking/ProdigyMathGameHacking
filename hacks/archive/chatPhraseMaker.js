cdba3ded36b104966040d07c60a11f0bc03bfe9c
SwxtchCode <swxtchcode@protonmail.com>
2020-10-23 05:15:36 -0700

    Moved broken hacks to a folder called "archive" (#334)
    * Moving broken hacks to "archived" folder.
    
    * Moving broken hacks to "archived" folder.
    
    * Update and rename hacks/misc/spam.js to hacks/archive/spam.js
    
    * Create README.md
    
    * Moving broken hacks to "archived" folder.
    
    This has been broken for a while.
    
    * Moving broken hacks to "archived" folder.
    
    * Moving broken hacks to "archived" folder.

--- hacks/archive/chatPhraseMaker.js
+++ hacks/archive/chatPhraseMaker.js
@@ -1,3 +1,6 @@
 //> Chat Phrase Maker
 //>> A custom chat _. Only client side.
+
+// NOTE: This hack is archived and no longer works.
+
 _.instance.prodigy.gameContainer.get("ChatManager").Y.push("INSERT TEXT HERE");
