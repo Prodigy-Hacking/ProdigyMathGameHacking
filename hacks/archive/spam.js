c8f3411cd25ddb89b5e1523545e8ac4cc6c90b91
Gale <>
2020-10-23 05:15:36 -0700

    Moved broken hacks to a folder called "archive" (#334)
    * Moving broken hacks to "archived" folder.
    
    * Moving broken hacks to "archived" folder.
    
    * Update and rename hacks/misc/spam.js to hacks/archive/spam.js
    
    * Create README.md
    
    * Moving broken hacks to "archived" folder.
    
    This has been broken for a while.
    
    * Moving broken hacks to "archived" folder.
    
    * Moving broken hacks to "archived" folder.

--- hacks/archive/spam.js
+++ hacks/archive/spam.js
@@ -1,5 +1,8 @@
 //> Effect Spammer
 //>> Spams confetti, snowballs, water balloons, and fireworks.
+
+// NOTE: This hack is archived and no longer works.
+
 setInterval(_ => {
 	for (let i of [1, 2, 3, 4]) {
 		_.instance.prodigy.gameContainer.get("NetworkManager").emitMessage({
@@ -8,8 +11,8 @@ setInterval(_ => {
 				type: i,
 				userID: _.player.userID,
 				x: Math.floor(Math.random() * 1280),
-				y: Math.floor(Math.random() * 720),
-			},
+				y: Math.floor(Math.random() * 720)
+			}
 		});
 	}
 });
