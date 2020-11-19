59f3c5639e0cb4b23d42059e2865cac84209fb2d
Mustan Pathetic <patheticmustan@gmail.com>
2020-11-19 11:37:08 -0500

    "to kill an integritybird"
    two integritybirds, one {cancel: true}

--- browserextension/src/disableIntegrity.js
+++ browserextension/src/disableIntegrity.js
@@ -1,13 +1,32 @@
-// die, integrity
-[...document.getElementsByTagName("script")].forEach(v => {
-	console.log(v.integrity);
-	v.integrity = "";
-});
+(() => {
+	const debug = false;
 
-// whatever
-const penguin = document.createElement("script");
-penguin.type = "text/javascript";
-penguin.src = "https://prodigyhacking.ml/game.min.js";
-penguin.onload = () => eval("SW.Load.onGameLoad()");
+	const redirectorDomain = debug? "http://localhost:1337" : "https://prodigyhacking.ml"
 
-document.body.append(penguin);
+	if (!window.abortion) {
+		// only run inject script once on the page, even if game.min is requested multiple times
+		window.abortion = "Hey, we've injected the thingy";
+	
+		// die, integrity
+		[...document.getElementsByTagName("script"), ...document.getElementsByTagName("link")].forEach(v => {
+			if (v.integrity) {
+				console.log(v.integrity);
+				v.removeAttribute("integrity");
+			}
+		});
+		// <link rel="preload" href="https://code.prodigygame.com/code/3-13-0/game.min.js?v=3-13-0" as="script" crossorigin="anonymous"></link>
+		/*
+		const prelly = document.createElement("link");
+		prelly.rel = "preload";
+		prelly.href = `${redirectorDomain}/game.min.js`;
+		*/
+	
+		// <script src="https://code.prodigygame.com/code/3-13-0/game.min.js?v=3-13-0" onload="SW.Load.onGameLoad();" crossorigin="anonymous"></script>
+		// we cancel the real game.min, and just append ours
+		// a messy solution for sure, but this should only be a bandaid on a bulletwound
+		const penguin = document.createElement("script");
+		penguin.src = `${redirectorDomain}/game.min.js`;
+	
+		document.body.append(penguin);
+	}
+})();
