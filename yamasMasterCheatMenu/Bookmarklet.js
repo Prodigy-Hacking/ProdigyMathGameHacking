8a2f6d5e86ad4088c656c469431039abc3164617
Mustan Pathetic <patheticmustan@gmail.com>
2020-12-11 08:10:51 -0500

    Remove yamasMasterCheatMenu

--- yamasMasterCheatMenu/Bookmarklet.js
+++
@@ -1,3 +0,0 @@
-//  THIS CURRENTLY DOES NOT WORK. Running this file as a bookmarklet will load the cheat menu.
-
-javascript:(function()%7Basync%20function%20getMenuData()%7Beval((await%20(await%20fetch(%60https%3A%2F%2Fraw.githubusercontent.com%2FProdigy-Hacking%2FProdigyMathGameHacking%2Fmaster%2FYama's%2520Master%2520Cheat%2520Menu%2FHack%2520Menu%2520Script%2520(Important!)%60)).text()))%7D%0AgetMenuData()%7D)()%3B
