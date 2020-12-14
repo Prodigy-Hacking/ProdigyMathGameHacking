4e28b6e5a64eb3a0e9a3fb3baa1ba8bfe2c9f8b7
Gale <>
2020-12-14 17:01:59 -0500

    fix morph hack default success
    forgot to add inputValidator, so invalid entries were let through >:(

--- willsCheatMenu/src/index.ts
+++ willsCheatMenu/src/index.ts
@@ -68,30 +68,39 @@ menuright.append(chat);
 
 export class Hack {
 	public element: HTMLButtonElement;
+	private name: String;
+	private description: String;
+
 	constructor(
 		public parent: HTMLDivElement,
 		name?: string,
 		description?: string
 	) {
+		this.name = "";
+		this.description = "";
 		this.element = document.createElement("button");
 		this.element.classList.add("menu-hack");
 		this.parent.append(this.element);
+
 		if (name) this.setName(name);
 		if (description) this.setDesc(description);
 	}
 	setName(name: string) {
 		this.element.innerText = name;
+		this.name = name;
 		return this;
 	}
 	setClick(event: () => unknown) {
 		this.element.onclick = (() => {
 			event();
 			_.player.forceSaveCharacter();
+			console.log(`Triggered ${this.name}.`);
 		});
 		return this;
 	}
 	setDesc(desc: string) {
 		this.element.title = desc;
+		this.description = desc;
 		return this;
 	}
 }
