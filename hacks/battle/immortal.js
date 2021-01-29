e53ac5292e089c2a1cc541881f9e0c8e79fb9183
NX721 <77288637+NX721@users.noreply.github.com>
2021-01-29 17:21:30 -0500

    Delete immortal.js
    Another Request For Acerion

--- hacks/battle/immortal.js
+++
@@ -1,6 +0,0 @@
-//> Immortal hack
-//>> Makes it so your health dropping below 0 doesn't even matter
-_.player.isKnockedOut = () => false;
-_.player.kennel.petTeam.forEach(pet => {
-  if(pet && pet.isKnockedOut) pet.isKnockedOut = () => false;
-});
