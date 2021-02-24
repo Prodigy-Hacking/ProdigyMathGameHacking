a150507946442c87d0f489f25f06d3395a4ab7d3
Arcerion <60945964+Avn1114@users.noreply.github.com>
2021-02-23 19:24:46 -0500

    yknow what mustan maybe we should try and do that cheat menu idea of yours

--- willsCheatMenu/src/hacks/inventory.ts
+++ willsCheatMenu/src/hacks/inventory.ts
@@ -32,7 +32,7 @@ new Hack(category.inventory, "Selector (Basic)").setClick(async () => {
 		if(!name) return;
 		if (!(await Confirm.fire(`Are you sure you want to get all ${name.toLowerCase()}?`)).value) return;
 		if (num === 14) {
-			_.gameData.dorm.forEach((x:any) =>
+			gameData.dorm.forEach((x:any) =>
 				_.player.house.data.items[x.ID] = { A: [], N: VERY_LARGE_NUMBER }
 			)
 			await Toast.fire("Furniture Added!", "All furniture have been added to your inventory!", "success");
@@ -74,6 +74,7 @@ new Hack(category.inventory, "Selector (Advanced)",'Choose a specific object and
 		}).then(async spec => {
 			let correct = parseInt(spec.value) + 1
 			if(!correct) return;
+			console.log(correct)
 			let amt = await NumberInput.fire("Amount", `How many of the object would you like?`, "question");
 			if(!amt.value) return;
 			if (correct === 14) {
