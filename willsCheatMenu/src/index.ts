5fb10a168caa68d794024f5d8d88a844309f1616
Arcerion <60945964+Avn1114@users.noreply.github.com>
2021-03-14 17:56:24 -0400

    gay

--- willsCheatMenu/src/index.ts
+++ willsCheatMenu/src/index.ts
@@ -21,7 +21,7 @@ toggler.onclick = () => {
 
 	if (visible) {
 		toggler.innerText = "▼";
-		menu.style.top = "-63vh";
+		menu.style.top = "-64vh";
 	} else {
 		toggler.innerText = "▲";
 		menu.style.top = "";
