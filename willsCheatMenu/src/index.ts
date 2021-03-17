f9fc4a0c8081c739f4b1c791ef7649d115069e73
ChickenOnYT <nameisa@prodigymathwiki.com>
2021-03-17 10:07:01 -0700

    Update index.ts

--- willsCheatMenu/src/index.ts
+++ willsCheatMenu/src/index.ts
@@ -52,8 +52,9 @@ const subtitle = document.createElement("h3");
 subtitle.style.fontSize = "20px";
 subtitle.innerHTML = `On behalf of <a href="https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/README.md">ProdigyMathGameHacking</a>. <a href="https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/README.md#Motivation">Learn about our mission</a>.
 <hr>
-This is free and open-source software. If you paid for this or accessed this behind a paywall/AdFly link, demand a refund. If you sell this software, or otherwise make a commercial advantage from it, you are violating Github conduct by not cooperating with our license.`;
-<p1><b>Created and maintained by the ProdigyMathGameHacking commmunity on Gitub.</p1></b>
+This is free and open-source software. If you paid for this or accessed this behind a paywall/AdFly link, demand a refund. If you sell this software, or otherwise make a commercial advantage from it, you are violating Github conduct by not cooperating with our license.
+
+<p1><b>Created and maintained by the ProdigyMathGameHacking commmunity on Gitub.</p1></b>`;
 menuleft.append(subtitle);
 
 export class Hack {
