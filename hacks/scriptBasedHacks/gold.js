d0971a3f62e53196b38377f3533e0de1ee79aa4f
hostedposted <hostedpostedsite@gmail.com>
2021-03-24 10:24:07 -0400

    Clean up parse jwt function

--- hacks/scriptBasedHacks/gold.js
+++ hacks/scriptBasedHacks/gold.js
@@ -1,10 +1,6 @@
 function parseJwt(token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
+    var base64 = token.split(".")[1];
+    var jsonPayload = atob(base64);
     return JSON.parse(jsonPayload);
 };
 
