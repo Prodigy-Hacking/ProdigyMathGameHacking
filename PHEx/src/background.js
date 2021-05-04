0bcfcf8b9eac9065a3c7db1aa64743896395b86b
Léo Badeaux <79180322+LeoBadeaux@users.noreply.github.com>
2021-05-04 13:08:37 -0400

    Add down message

--- PHEx/src/background.js
+++ PHEx/src/background.js
@@ -17,6 +17,16 @@ browser.webRequest.onBeforeRequest.addListener(details => {
 		chrome.tabs.executeScript({
 			file: "disableIntegrity.js"
 		});
+		
+	fetch('https://raw.githubusercontent.com/Prodigy-Hacking/ProdigyMathGameHacking/master/PHEx/status.json').then(response => response.json()).then(data => {
+		if (data.offline == true) {
+			chrome.notifications.create({
+ 			 "type": "basic",
+ 			 "title": "Hacks are down,
+ 			 "message": "Our hacks are currently having some issues, and we're working on it."
+			});
+		}
+	});
 
 		// see disableIntegrity.js, we append the new game.min to the document
 
