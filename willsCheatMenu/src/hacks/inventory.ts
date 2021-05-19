2bae4d025df276e6d588d0b3fc895279439a6d04
Gale <>
2021-05-18 20:03:11 -0400

    subtract equal not equal

--- willsCheatMenu/src/hacks/inventory.ts
+++ willsCheatMenu/src/hacks/inventory.ts
@@ -189,7 +189,7 @@ new Hack(category.inventory, "Remove item").setClick(async () => {
 	const amt = await NumberInput.fire("Amount", "How many of the object would you like to remove?", "question");
 	if(!amt.value) return;
 	if (_.player.backpack.data[ids[category.value]].findIndex(e => e.ID === _.gameData[ids[category.value]][item].ID) === -1) {
-		_.player.backpack.data[ids[category.value]][item].N = amt.value;
+		_.player.backpack.data[ids[category.value]][item].N -= amt.value;
 	}
 
 	await Toast.fire("Removed!", `Successfully removed ${amt.value} ${_.gameData[ids[category.value]][item].name}!`, "success");
