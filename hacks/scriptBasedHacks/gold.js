434f1c9a119b76c242f14fce5362b9e572aa677a
ArcerionDev <arcerion114@gmail.com>
2021-05-25 16:43:44 -0400

    Fix script based hacks

---
+++ hacks/scriptBasedHacks/gold.js
@@ -0,0 +1,28 @@
+function parseJwt(token) {
+    var base64 = token.split(".")[1];
+    var jsonPayload = atob(base64);
+    return JSON.parse(jsonPayload);
+};
+
+let userID = parseJwt(localStorage.JWT_TOKEN).content.userID
+let playerdata = await (await fetch(`https://api.prodigygame.com/game-api/v1/character/${userID}?isMember=0&userID=${userID}`, {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+        'Authorization': localStorage.JWT_TOKEN,
+
+    },
+})).json();
+
+playerdata.data.gold = 100000000000;
+
+await fetch(`https://api.prodigygame.com/game-api/v3/characters/${userID}`, {
+    headers: {
+        "Authorization": localStorage.JWT_TOKEN
+    }, 
+    body: JSON.stringify({
+        data: JSON.stringify(playerdata),
+        userID: userID
+    }),
+    method: "POST"
+})
