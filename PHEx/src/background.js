bcc616ab91443051bd98e7b6b82abc165eb46dfe
Gale <>
2021-05-29 13:08:22 -0400

    Big whoopsie

--- PHEx/src/background.js
+++ PHEx/src/background.js
@@ -22,7 +22,7 @@ const HEADERS_TO_STRIP_LOWERCASE = [
 
 // Redirect Requests
 browser.webRequest.onBeforeRequest.addListener(details => {
-	const redirectorDomain = debug ? "http://localhost:1337" : "https://prodigyhacking.ml"
+	const redirectorDomain = debug ? "http://localhost:1337" : "https://hacks.prodigyhacking.com"
 	if (details.url.startsWith("https://code.prodigygame.com/code/") && details.url.includes("/game.min.js")) {
 	// instead of redirecting
 		chrome.tabs.executeScript({
