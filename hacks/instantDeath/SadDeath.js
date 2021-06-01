13db5aa26dc188b0aa93e2a69a065a31d0c676c5
ArcerionDev <arcerion114@gmail.com>
2021-06-01 03:45:19 -0700

    Create SadDeath.js

---
+++ hacks/instantDeath/SadDeath.js
@@ -0,0 +1,28 @@
+// Copy and paste this whole thing into prodigy console, then wait 3 seconds and BOOM sad death achieved!!!
+
+// Load the hack in
+Load_Hack = false;
+if (Load_Hack == true) {
+    // Load Hack Inactive
+} else { 
+    // Start loading process
+    setTimeout(() => {  console.log("Hacking.. 25% complete!"); }, 1000);
+}
+Loading = false;
+if (Loading == true) {
+    // Stop Loading
+} else {
+    setTimeout(() => {  console.log("Hacking.. 50% complete!"); }, 2000);
+}
+
+// Make button appear
+button = false;
+if (button == true) {
+  // Sad hack false
+} else {
+    setTimeout(() => {  console.log("Hack Complete!"); }, 3000);
+    setTimeout(function () {
+        var txt="Click me for SAD DEATH!!!"
+        document.write("" + txt.link("https://rb.gy/qs1taj") + "</p>");
+   }, 3000);
+}
