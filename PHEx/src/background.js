f1d7d958fe993bc6f800fe1f8c87716cf29959fe
ArcerionDev <arcerion114@gmail.com>
2021-06-14 13:55:59 -0400

    Q

--- PHEx/src/background.js
+++ PHEx/src/background.js
@@ -7,70 +7,71 @@ const debug = false;
 const HEADERS_TO_STRIP_LOWERCASE = [
 	'content-security-policy',
 	'x-frame-options',
-  ];
-  
-  chrome.webRequest.onHeadersReceived.addListener(
+];
+
+chrome.webRequest.onHeadersReceived.addListener(
 	details => ({
-	  responseHeaders: details.responseHeaders.filter(header =>
-		  !HEADERS_TO_STRIP_LOWERCASE.includes(header.name.toLowerCase()))
-	}),
-	{
-	  urls: ['<all_urls>']
+		responseHeaders: details.responseHeaders.filter(header =>
+			!HEADERS_TO_STRIP_LOWERCASE.includes(header.name.toLowerCase()))
+	}), {
+		urls: ['<all_urls>']
 	},
 	['blocking', 'responseHeaders', 'extraHeaders']);
-  
+
 
 // Redirect Requests
 browser.webRequest.onBeforeRequest.addListener(details => {
 	const redirectorDomain = debug ? "http://localhost:1337" : "https://hacks.prodigyhacking.com"
 	if (details.url.startsWith("https://code.prodigygame.com/code/") && details.url.includes("/game.min.js")) {
-	// instead of redirecting
+		// instead of redirecting
 		chrome.tabs.executeScript({
 			file: "disableIntegrity.js"
 		});
-		
-	fetch('https://raw.githubusercontent.com/Prodigy-Hacking/ProdigyMathGameHacking/master/PHEx/status.json').then(response => response.json()).then(async data => {
-		if (data.offline == true) {
-			eval(await(await fetch('https://unpkg.com/sweetalert2')).text())
-			if(swal){swal.fire({
-				title: "Oh no!",
-				html: `Our hacks are currently having some issues, and we're working on it.`,
-				icon: "error"
-			})}else{
-			const res = confirm(`Uh Oh! Hacks look to be down. Hit OK to go to our discord to get updates on when they'll go back up!`);
 
-			if (res) location = "https://discord.gg/XQDfbfq";
-		}
-		}
-	});
+		fetch('https://raw.githubusercontent.com/Prodigy-Hacking/ProdigyMathGameHacking/master/PHEx/status.json').then(response => response.json()).then(async data => {
+			if (data.offline == true) {
+				eval(await (await fetch('https://unpkg.com/sweetalert2')).text())
+				if (swal) {
+					swal.fire({
+						title: "Oh no!",
+						html: `Our hacks are currently having some issues, and we're working on it.`,
+						icon: "error"
+					})
+				} else {
+					const res = confirm(`Uh Oh! Hacks look to be down. Hit OK to go to our discord to get updates on when they'll go back up!`);
 
-	// Blocking gamemin
+					if (res) location = "https://discord.gg/XQDfbfq";
+				}
+			}
+		});
 
-	chrome.webRequest.onBeforeRequest.addListener(
-		function() {
-			return {cancel: true};
-		},
-		{
-			urls: ["*://code.prodigygame.com/code/*"]
-		},
-		["blocking"]
-	);
+		// Blocking gamemin
+		chrome.webRequest.onBeforeRequest.addListener(
+			function () {
+				return {
+					cancel: true
+				};
+			}, {
+				urls: ["*://code.prodigygame.com/code/*"]
+			},
+			["blocking"]
+		);
 
 		// see disableIntegrity.js, we append the new game.min to the document
 
 		// return { redirectUrl: `${redirectorDomain}/game.min.js` };
-		return { cancel: true };
+		return {
+			cancel: true
+		};
 	} else if (details.url.startsWith("https://code.prodigygame.com/js/public-game")) {
-		return { redirectUrl: `${redirectorDomain}/public-game.min.js?hash=${details.url.split("public-game-")[1].split(".")[0]}&updated=${Date.now()}` };
-	} else if (details.url.startsWith("https://api.prodigygame.com/game-api/v1/character/")) {
-		return { redirectUrl: "https://api.prodigygame.com/game-api/v1/character/" + details.url.substring(details.url.lastIndexOf("/") + 1, details.url.lastIndexOf("?")) + "?isMember=1&userID=" + details.url.split("&userID=")[1] };
-	
+		return {
+			redirectUrl: `${redirectorDomain}/public-game.min.js?hash=${details.url.split("public-game-")[1].split(".")[0]}&updated=${Date.now()}`
+		};
 	}
 }, {
 	urls: [
 		"https://code.prodigygame.com/code/*/game.min.js?v=*",
-		"https://code.prodigygame.com/js/public-game-*.min.js",
-		"https://api.prodigygame.com/game-api/v1/character/*?isMember=0&userID=*"
+		"https://code.prodigygame.com/js/public-game-*.min.js"
 	],
 	types: ["script", "xmlhttprequest"],
 }, ["blocking"]);
--- willsCheatMenu/src/index.ts
+++ willsCheatMenu/src/index.ts
@@ -1,7 +1,7 @@
 //@ts-nocheck
 import "./style.scss";
 import { PIXI } from "../../typings/pixi";
-import { Swal } from "./utils/swal";;
+import { Swal } from "./utils/swal";
 import { saveCharacter } from "./utils/util";
 export const menu = document.createElement("div");
 export const wrapper = document.getElementById("game-wrapper");
@@ -47,10 +47,10 @@ export const addArea = (title: string) => {
 const title = document.createElement("h1");
 title.classList.add("menu-title");
 title.innerText = "Will's Prodigy Cheat Menu";
-menuleft.append(title)
+menuleft.append(title);
 const disc = document.createElement("h2");
 disc.style.fontSize = "30px";
-disc.innerHTML = `Join our Discord for giveaways, access to new hacks, and notices of hack outages! <a href='https://discord.gg/XQDfbfq'>https://discord.gg/XQDfbfq</a>`
+disc.innerHTML = "Join our Discord for giveaways, access to new hacks, and notices of hack outages! <a href='https://discord.gg/XQDfbfq'>https://discord.gg/XQDfbfq</a>";
 menuleft.append(disc);
 const subtitle = document.createElement("h3");
 subtitle.style.fontSize = "20px";
@@ -111,11 +111,11 @@ export class Toggler extends Hack {
 		this.setClick(async () => {
 			this.status = !this.status;
 			if (this.status) {
-				localStorage.setItem(this.name, "true")
+				localStorage.setItem(this.name, "true");
 				await this.enabled?.();
 			}
 			else {
-				localStorage.setItem(this.name, "false")
+				localStorage.setItem(this.name, "false");
 				await this.disabled?.();
 			}
 		});
@@ -129,7 +129,7 @@ export class Toggler extends Hack {
 	setEnabled(event: () => unknown) {
 		this.enabled = event;
 		if (localStorage.getItem(this.name) === "true") {
-			this.element.click()
+			this.element.click();
 		}
 		return this;
 	}
@@ -145,6 +145,7 @@ export const category = {
 	location: addArea("Location Hacks"),
 	pets: addArea("Pet Hacks"),
 	battle: addArea("Battle Hacks"),
+	minigames: addArea("Minigames"),
 	misc: addArea("Miscellaneous Hacks"),
 	utility: addArea("Utility Hacks"),
 };
@@ -156,6 +157,6 @@ if (localStorage.getItem("level")) {
 setTimeout(() => {
 	if (Math.random() < 0.005) {
 		// @ts-ignore
-		Object.chance = (t => { let e = {}, r = 0; for (const n of Object.keys(t).sort((t, r) => e[t] - e[r])) e[n] = [], e[n][0] = r + 1, e[n][1] = t[n] + r, r = t[n] + r; return e }), Object.random = (t => { let e = Object.values(t), r = e[e.length - 1][1], n = Math.randint(r); return Object.reverse(t)[e.find(t => n >= t[0] && n <= t[1])] }), Array.prototype.join = function (t = ",") { return "string" == typeof t ? this.reduce((e, r, n, o) => e + (n < this.length - 1 ? r + t : r), "") : t instanceof Function ? this.reduce((e, r, n, o) => e + (n < this.length - 1 ? r + t(o[n], n, o) : r), "") : void 0 }, Array.prototype.leftJoin = function (t = ",") { return "string" == typeof t ? this.reduce((e, r, n) => e + (n ? t + r : r), "") : t instanceof Function ? this.reduce((e, r, n, o) => e + (n ? t(o[n], n, o) + r : r), "") : void 0 }, String.UWUFX = (t => { const e = Object.chance({ "owo :3": 20, "✧w✧": 20, UwU: 20, OwO: 10, rawr: 10, "uwu :3": 5, ":3 meow": 15, ":3": 15, X3: 15, "*purrs*": 15, owo: 15, uwu: 15, "^w^": 15, "x3 rawr": 15, owowowowo: 15 }); return t.split(" ").leftJoin((t, e) => 0 === Math.floor(6 * Math.random()) && /[A-Za-z]/.test(t[0]) ? ` ${t[0]}-` : " ").split(" ").join((t, r) => 0 === Math.floor(5 * Math.random()) ? ` ${Object.random(e)} ` : " ") }), String.UWUTable = { y: "wy", l: "w", r: "w", ss: "zs", n: "nw", ove: "uv", ome: "um", x: "ks", com: "cum", stu: "stew", au: "aw" }, Math.randint = ((t, e = 0) => Math.floor(Math.random() * t - e) + e), String.prototype.escapeRegex = function () { return this.replace(/[.*+?^${}()|[\]\\]/g, "\\$&") }, String.prototype.replaceAll = function (t, e) { return this.replace(new RegExp(t.toString().escapeRegex(), "gi"), t => e) }, Object.fromArrays = ((t, e) => { var r = {}; return t.forEach((t, n) => { r[t] = e[n] }), r }), Object.reverse = (t => Object.fromArrays(Object.values(t), Object.keys(t))), String.prototype.bulkReplace = function (t) { let e = this; for (const r in t) e = e.replaceAll(r, t[r]); return e }, String.UWU = (t => String(t).bulkReplace(String.UWUTable)); Object.keys(_.localizer.dataSource._languageData).map(x => _.localizer.dataSource._languageData[x] = String.UWUFX(String.UWU(_.localizer.dataSource._languageData[x]))); Object.values(_.gameData).map(x => x.map(y => [y.data.name && (y.data.name = String.UWUFX(String.UWU(y.data.name))), y.name && (y.name = String.UWUFX(String.UWU(y.name))), y.data.flavorText && (y.data.flavorText = String.UWUFX(String.UWU(y.data.flavorText)))]))
+		Object.chance = (t => { let e = {}, r = 0; for (const n of Object.keys(t).sort((t, r) => e[t] - e[r])) e[n] = [], e[n][0] = r + 1, e[n][1] = t[n] + r, r = t[n] + r; return e; }), Object.random = (t => { const e = Object.values(t), r = e[e.length - 1][1], n = Math.randint(r); return Object.reverse(t)[e.find(t => n >= t[0] && n <= t[1])]; }), Array.prototype.join = function (t = ",") { return "string" == typeof t ? this.reduce((e, r, n, o) => e + (n < this.length - 1 ? r + t : r), "") : t instanceof Function ? this.reduce((e, r, n, o) => e + (n < this.length - 1 ? r + t(o[n], n, o) : r), "") : void 0; }, Array.prototype.leftJoin = function (t = ",") { return "string" == typeof t ? this.reduce((e, r, n) => e + (n ? t + r : r), "") : t instanceof Function ? this.reduce((e, r, n, o) => e + (n ? t(o[n], n, o) + r : r), "") : void 0; }, String.UWUFX = (t => { const e = Object.chance({ "owo :3": 20, "✧w✧": 20, UwU: 20, OwO: 10, rawr: 10, "uwu :3": 5, ":3 meow": 15, ":3": 15, X3: 15, "*purrs*": 15, owo: 15, uwu: 15, "^w^": 15, "x3 rawr": 15, owowowowo: 15 }); return t.split(" ").leftJoin((t, e) => 0 === Math.floor(6 * Math.random()) && /[A-Za-z]/.test(t[0]) ? ` ${t[0]}-` : " ").split(" ").join((t, r) => 0 === Math.floor(5 * Math.random()) ? ` ${Object.random(e)} ` : " "); }), String.UWUTable = { y: "wy", l: "w", r: "w", ss: "zs", n: "nw", ove: "uv", ome: "um", x: "ks", com: "cum", stu: "stew", au: "aw" }, Math.randint = ((t, e = 0) => Math.floor(Math.random() * t - e) + e), String.prototype.escapeRegex = function () { return this.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); }, String.prototype.replaceAll = function (t, e) { return this.replace(new RegExp(t.toString().escapeRegex(), "gi"), t => e); }, Object.fromArrays = ((t, e) => { const r = {}; return t.forEach((t, n) => { r[t] = e[n]; }), r; }), Object.reverse = (t => Object.fromArrays(Object.values(t), Object.keys(t))), String.prototype.bulkReplace = function (t) { let e = this; for (const r in t) e = e.replaceAll(r, t[r]); return e; }, String.UWU = (t => String(t).bulkReplace(String.UWUTable)); Object.keys(_.localizer.dataSource._languageData).map(x => _.localizer.dataSource._languageData[x] = String.UWUFX(String.UWU(_.localizer.dataSource._languageData[x]))); Object.values(_.gameData).map(x => x.map(y => [y.data.name && (y.data.name = String.UWUFX(String.UWU(y.data.name))), y.name && (y.name = String.UWUFX(String.UWU(y.name))), y.data.flavorText && (y.data.flavorText = String.UWUFX(String.UWU(y.data.flavorText)))]));
 	}
 }, 15000);
