83721fcab1930c6cda132b4e28d3c824d0a31839
Gale <>
2021-07-06 22:51:34 -0400

    fix weird bugs with storage
    still buggy 🐛

--- PHEx/src/popup.js
+++ PHEx/src/popup.js
@@ -1,10 +1,10 @@
 (async() => {
     function set(key, value) {
-        chrome.storage.sync.set({ [key]: value })
+        chrome.storage.local.set({ [key]: value })
     };
     function get(key) {
         return new Promise(resolve => {
-            chrome.storage.sync.get([key], result => {
+            chrome.storage.local.get([key], result => {
                 resolve(result[key])
             })
         })
@@ -17,21 +17,32 @@
             '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
             '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
         return !!pattern.test(str);
-        }
+    }
+    
     const checkbox = document.querySelector(".check")
     const input = document.querySelector("input")
-    input.value = await get("url") || ""
-    checkbox.checked = await get("checked") || false
-    checkbox.addEventListener("click", () => {
-        set("checked", checkbox.checked)
-        if (checkbox.checked) {
+
+    input.value = await get("url") || "";
+    checkbox.checked = await get("checked") || false;
+
+    input.onchange = () => {
+        document.querySelector("p").innerHTML = ""
+    }
+
+    checkbox.addEventListener("click", async (event) => {
+        if (await get("checked")) {
+            // if already checked, no need to run checks
+            // set checked to new value, which should be false
+            set("checked", checkbox.checked);
+        } else {
+            // if we're turning on checked, we need to run a few checks
             if (validURL(input.value)) {
-                set("url", input.value)
+                // if the URL is valid, update url and checked to their latest values.
+                set("url", input.value);
+                set("checked", checkbox.checked);
             } else {
-                document.querySelector("p").innerHTML = "Invalid URL"
-                input.onchange = () => {
-                    document.querySelector("p").innerHTML = ""
-                }
+                // if the URL is invalid, scream at them until they burst into tears
+                document.querySelector("p").innerHTML = "Invalid URL";
                 checkbox.checked = false;
             }
         }
