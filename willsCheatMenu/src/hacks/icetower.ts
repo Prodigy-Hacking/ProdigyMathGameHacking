9f07349c613fef62f07705646d8e3d1c287ed23e
hostedposted <hostedpostedsite@gmail.com>
2021-07-07 17:35:52 -0400

    Apparently Fix Ice Keystone is in the pets category

--- willsCheatMenu/src/hacks/icetower.ts
+++ willsCheatMenu/src/hacks/icetower.ts
@@ -6,7 +6,7 @@ import { TODO } from "../../../typings/util";
 import { prodigy, game } from "../utils/util";
 
 
-new Hack(category.pets, "Fix Ice Keystone").setClick(async () => {
+new Hack(category.icetower, "Fix Ice Keystone").setClick(async () => {
 	_.instance.prodigy.world.zones.icetower._towerData.enabled = true
 _.instance.prodigy.world.zones.icetower.update()
 	await Toast.fire("Success!", "Ice Keystone has been fixed.", "success");
