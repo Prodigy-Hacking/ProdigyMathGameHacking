c77aefa3c0481ab58c3de0910f00370add46d66b
ArcerionDev <arcerion114@gmail.com>
2021-07-07 11:43:49 -0400

    Icetower

---
+++ willsCheatMenu/src/hacks/icetower.ts
@@ -0,0 +1,11 @@
+//@ts-nocheck
+import { Swal, Toast, NumberInput, Input, Confirm } from "../utils/swal";
+import { Hack, category } from "../index";
+import { VERY_LARGE_NUMBER, gameData, pickRandom } from "../utils/util";
+import { TODO } from "../../../typings/util";
+import { prodigy, game } from "../utils/util";
+new Hack(category.pets, "Fix Ice Keystone").setClick(async () => {
+	_.instance.prodigy.world.zones.icetower._towerData.enabled = true
+_.instance.prodigy.world.zones.icetower.update()
+	await Toast.fire("Success!", "Ice Keystone has been fixed.", "success");
+});
