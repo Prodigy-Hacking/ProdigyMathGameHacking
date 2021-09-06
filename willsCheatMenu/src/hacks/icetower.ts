3cb9ef8104244f41d5798fcda82af0c1105bd599
donaldli2020 <donaldli2020@outlook.com>
2021-09-06 17:02:39 -0400

    add other towers
    tested

--- willsCheatMenu/src/hacks/icetower.ts
+++ willsCheatMenu/src/hacks/icetower.ts
@@ -8,3 +8,18 @@ new Hack(category.icetower, "Fix Ice Keystone").setClick(async () => {
 	_.instance.prodigy.world.zones.icetower.update();
 	await Toast.fire("Success!", "Ice Keystone has been fixed.", "success");
 });
+new Hack(category.icetower, "Fix Storm Keystone").setClick(async () => {
+	_.instance.prodigy.world.zones.stormtower._towerData.enabled = true;
+	_.instance.prodigy.world.zones.stormtower.update();
+	await Toast.fire("Success!", "Storm Keystone has been fixed.", "success");
+});
+new Hack(category.icetower, "Fix Fire Keystone").setClick(async () => {
+	_.instance.prodigy.world.zones.firetower._towerData.enabled = true;
+	_.instance.prodigy.world.zones.firetower.update();
+	await Toast.fire("Success!", "Fire Keystone has been fixed.", "success");
+});
+new Hack(category.icetower, "Fix Water Keystone").setClick(async () => {
+	_.instance.prodigy.world.zones.watertower._towerData.enabled = true;
+	_.instance.prodigy.world.zones.watertower.update();
+	await Toast.fire("Success!", "Ice Keystone has been fixed.", "success");
+});
