f66324a8212d7f6f700a9c537c6c8f0f72e6c518
Arcerion <60945964+ArcerionDev@users.noreply.github.com>
2021-09-27 19:32:15 -0400

    when all else fails wrap everything in an anonymous async

--- willsCheatMenu/dist/bundle.js
+++ willsCheatMenu/dist/bundle.js
@@ -1,2 +1,3 @@
-eval(await (await fetch("https://unpkg.com/sweetalert2")).text())
+(async() => {eval(await (await fetch("https://unpkg.com/sweetalert2")).text())
 if (swal) {swal.fire({title: "Hey!", html: `Our hacks are currently having some issues, and we're disabling them until we get them running as smoothly as possible. Please disable your extension until further notice to keep playing Prodigy. Thanks for understanding ❤<br>Join our Discord at <a href="https://discord.gg/XQDfbfq">https://discord.gg/XQDfbfq</a> for info on when they'll be back!`,icon: "info"}).then(e => {document.location.reload()})}
+})()
