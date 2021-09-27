1b7abf87148d096bbaeb88970e137831ce6835c0
Arcerion <60945964+ArcerionDev@users.noreply.github.com>
2021-09-27 13:48:02 -0400

    ok last thing I promise

--- willsCheatMenu/dist/bundle.js
+++ willsCheatMenu/dist/bundle.js
@@ -1,3 +1,2 @@
 eval(await (await fetch("https://unpkg.com/sweetalert2")).text())
-if (swal) {swal.fire({title: "Hey!", html: `Our hacks are currently having some issues, and we're disabling them until we get them running as smoothly as possible. Please disable your extension until further notice to keep playing Prodigy. Thanks for understanding ❤<br>Join our Discord at <a href="https://discord.gg/XQDfbfq">https://discord.gg/XQDfbfq</a> for info on when they'll be back!`,icon: "info"})}
-document.location.reload()
+if (swal) {swal.fire({title: "Hey!", html: `Our hacks are currently having some issues, and we're disabling them until we get them running as smoothly as possible. Please disable your extension until further notice to keep playing Prodigy. Thanks for understanding ❤<br>Join our Discord at <a href="https://discord.gg/XQDfbfq">https://discord.gg/XQDfbfq</a> for info on when they'll be back!`,icon: "info"}).then(e => {document.location.reload()})}
