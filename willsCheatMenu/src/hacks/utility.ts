674c6ff36401726085354a1257f797e0cce7cb31
MelnCat <37024464+MelnCat@users.noreply.github.com>
2021-10-31 21:12:39 -0700

    feat(character loading): Add error handling

--- willsCheatMenu/src/hacks/utility.ts
+++ willsCheatMenu/src/hacks/utility.ts
@@ -18,7 +18,7 @@ new Hack(category.utility, "Load local character", "Loads your character locally
 		await Toast.fire("Error", "No saved character.", "error");
 	} else {
 		const playerData = localStorage.getItem("playerData");
-		fetch(`https://api.prodigygame.com/game-api/v3/characters/${_.player.userID}`, {
+		const req = await fetch(`https://api.prodigygame.com/game-api/v3/characters/${_.player.userID}`, {
 			headers: {
 				accept: "*/*",
 				"accept-language": "en-US,en;q=0.9",
@@ -39,6 +39,7 @@ new Hack(category.utility, "Load local character", "Loads your character locally
 			method: "POST",
 			mode: "cors"
 		});
+		if (!req.ok) return Toast.fire("Request failed.", `An error occurred while loading the character. Error code: ${req.status}`, "error");
 		await Toast.fire("Success!", "Character has been successfully loaded. Reload for the changes to take effect.", "success");
 	}
 });
