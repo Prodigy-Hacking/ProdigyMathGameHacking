33eebd10806389edefb0956ee357107dd98846bf
MelnCat <37024464+MelnCat@users.noreply.github.com>
2021-10-31 21:40:17 -0700

    fix(click teleporting): Fix error with error handling.

--- willsCheatMenu/src/hacks/utility.ts
+++ willsCheatMenu/src/hacks/utility.ts
@@ -94,7 +94,7 @@ new Toggler(category.utility, "Toggle Click Teleporting").setEnabled(async () =>
 		try {
 			_.player._playerContainer.walkSpeed = 500;
 		} catch (e) { 
-			await Toast.fire("An error occurred.", `An error occurred while toggling click teleporting. Error: ${e}`, "error");
+			Toast.fire("An error occurred.", `An error occurred while toggling click teleporting. Error: ${e}`, "error").catch(console.error);
 			clearInterval(teleportingInterval);
 		}
 	});
