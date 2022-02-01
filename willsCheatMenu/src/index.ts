149db8db22a652ab88fa65b50c2e056893bb95aa
ArcerionDev <arcerion114@gmail.com>
2022-02-01 16:23:39 -0500

    Make show/hide menu actually work

--- willsCheatMenu/src/index.ts
+++ willsCheatMenu/src/index.ts
@@ -53,13 +53,14 @@ title.innerText = "Will's Prodigy Cheat Menu";
 menuleft.append(title);
 const disc = document.createElement("h2");
 disc.style.fontSize = "30px";
-disc.innerHTML = "Join our Discord for giveaways, access to new hacks, and notices of hack outages! <a href='https://discord.gg/XQDfbfq'>https://discord.gg/XQDfbfq</a>";
+disc.innerHTML = "Join our Discord for giveaways, access to new hacks, and notices of hack outages! <a href='https://discord.gg/XQDfbfq'>https://discord.gg/XQDfbfq</a> <br> Press SHIFT to show/hide the menu.";
 menuleft.append(disc);
 const subtitle = document.createElement("h3");
 subtitle.style.fontSize = "20px";
 subtitle.innerHTML = `On behalf of <a href="https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/README.md">ProdigyMathGameHacking</a>. <a href="https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/README.md#Motivation">Learn about our mission</a>.
 <hr>
-This is free and open-source software. If you paid for this or accessed this behind a paywall/AdFly link, demand a refund. If you sell this software, or otherwise make a commercial advantage from it, you are violating Github conduct by not cooperating with our license.`;
+This is free and open-source software. If you paid for this or accessed this behind a paywall/AdFly link, demand a refund. If you sell this software, or otherwise make a commercial advantage from it, you are violating Github conduct by not cooperating with our license.
+`;
 menuleft.append(subtitle);
 
 export class Hack {
@@ -179,8 +180,8 @@ if (localStorage.getItem("level")) {
 	_.player.getLevel = () => localStorage.getItem("level");
 }
 
-document.addEventListener("keypress", function (event) {
-	if (event.keyCode == 16) {
+document.addEventListener("keydown", function (event) {
+	if (event.key == "Shift") {
 		if (document.getElementById("cheat-menu").style.display == "block" && document.getElementById("menu-toggler").style.display == "block") {
 			// Cheats are shown, so let's hide them.
 			document.getElementById("cheat-menu").style.display = "none";
