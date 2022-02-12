5b3b7ff134fc8ccaed77fa59d848c3dd264edc68
hostedposted <hostedpostedsite@gmail.com>
2022-02-12 15:55:25 -0500

    Make menu look better

--- PHEx/src/popup.js
+++ PHEx/src/popup.js
@@ -1,15 +1,17 @@
-(async() => {
-    function set(key, value) {
-        chrome.storage.local.set({ [key]: value })
-    };
-    function get(key) {
-        return new Promise(resolve => {
-            chrome.storage.local.get([key], result => {
-                resolve(result[key])
-            })
+function set(key, value) {
+    chrome.storage.local.set({ [key]: value })
+};
+
+function get(key) {
+    return new Promise(resolve => {
+        chrome.storage.local.get([key], result => {
+            resolve(result[key])
         })
-    };
-    function validURL(str) {
+    })
+};
+
+function validURL(str) {
+    try {
         var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
             '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
             '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
@@ -17,34 +19,42 @@
             '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
             '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
         return !!pattern.test(str) || new URL(str).hostname === "localhost";
+    } catch {
+        return false
     }
-    
-    const checkbox = document.querySelector(".check")
-    const input = document.querySelector("input")
+}
 
-    input.value = await get("url") || "";
-    checkbox.checked = await get("checked") || false;
+const developerForm = document.querySelector("form#dev")
+const revealButton = document.getElementById("reveal-button")
+const checkbox = document.querySelector("input#checkbox")
+const pnpUrl = document.querySelector("input#pnp-url")
+const alert = document.getElementById("alert")
+const closeButton = document.getElementById("close-button")
 
-    input.onchange = () => {
-        document.querySelector("p").innerHTML = ""
+revealButton.addEventListener("click", () => {
+    if (developerForm.classList.length === 1) {
+        developerForm.classList.remove("hidden")
+    } else {
+        developerForm.classList.add("hidden")
     }
+});
 
-    checkbox.addEventListener("click", async (event) => {
-        if (await get("checked")) {
-            // if already checked, no need to run checks
-            // set checked to new value, which should be false
-            set("checked", checkbox.checked);
+checkbox.addEventListener("change", () => {
+    if (!validURL(pnpUrl.value)) {
+        if (checkbox.checked) {
+            alert.classList.remove("hidden")
         } else {
-            // if we're turning on checked, we need to run a few checks
-            if (validURL(input.value)) {
-                // if the URL is valid, update url and checked to their latest values.
-                set("url", input.value);
-                set("checked", checkbox.checked);
-            } else {
-                // if the URL is invalid, scream at them until they burst into tears
-                document.querySelector("p").innerHTML = "Invalid URL";
-                checkbox.checked = false;
-            }
+            alert.classList.add("hidden")
         }
-    })
-})()
+        checkbox.checked = false
+        set("url", "")
+        set("checked", false)
+        return
+    }
+    set("url", pnpUrl.value)
+    set("checked", checkbox.checked)
+})
+
+closeButton.addEventListener("click", () => {
+    alert.classList.add("hidden")
+})
